aabf3caf6f49e665c3027ef4825ea3a4b71f522b
Michael Blocksome <blocksom@us.ibm.com>
2009-09-02 11:08:36 -0500

    Update bgp platform include.

--- api/c/bgp/platform.h
+++ api/c/bgp/platform.h
@@ -1,4 +1,4 @@
-#include "components/client/bgp/bgpclient.h"
-#include "components/context/bgp/bgpcontext.h"
+#include "components/client/bgp/BgpClient.h"
+#include "components/context/bgp/BgpContext.h"
 #include "components/pipeworkqueue/bgp/BgpPipeWorkQueue.h"
 #include "components/topology/bgp/BgpTopology.h"
