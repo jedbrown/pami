f100d713c337d45762c82417934901ad1806bd71
Charles Archer <archerc@duo.rchland.ibm.com>
2009-09-09 12:46:51 -0500

    added registration class

--- components/geometry/mpi/mpicollregistration.h
+++ components/geometry/mpi/mpicollregistration.h
@@ -3,7 +3,7 @@
 
 #define XMI_COLLREGISTRATION_CLASS XMI::CollRegistration::MPI
 
-
+#include <map>
 #include "components/geometry/CollRegistration.h"
 #include "components/devices/mpi/oldmpimulticastmodel.h"
 #include "components/devices/mpi/mpimessage.h"
@@ -30,9 +30,10 @@ namespace XMI
       inline MPI(XMI::Device::MPIDevice<XMI::SysDep::MPISysDep> *dev):
         CollRegistration<XMI::CollRegistration::MPI<T_Geometry, T_Collfactory>, T_Geometry, T_Collfactory>()
         {
-          // Collective Protocol 0
-
-          XMI_NBCollManager::instance()->multisend_reg(TSPColl::BcastTag, &_pgasrt_mcast);
+          // Pgasrt broadcast call
+          XMI_NBCollManager::instance()->multisend_reg(TSPColl::BcastTag, &_pgasrt_broadcast);
+          
+          
           
 #if 0
 //          XMI::Device::MPIDevice<MPISysDep> *dev
@@ -56,8 +57,21 @@ namespace XMI
         }
 
     public:
-      MPIMcastModel _ccmi_mcast;
-      MPIMcastModel _pgasrt_mcast;
+      std::map<MPIMcastModel*, int> collectiveid;
+      MPIMcastModel _ccmi_broadcast;
+      MPIMcastModel _pgasrt_broadcast;
+      MPIMcastModel _pgasrt_allgather;
+      MPIMcastModel _pgasrt_allgatherv;
+      MPIMcastModel _pgasrt_scatter;
+      MPIMcastModel _pgasrt_scatterv;
+      MPIMcastModel _ccmi_allreduce;
+      MPIMcastModel _pgasrt_allreduce;
+      MPIMcastModel _ccmi_alltoall;
+      MPIMcastModel _ccmi_barrier;
+      MPIMcastModel _pgasrt_barrier;
+      MPIMcastModel _ccmi_ambroadcast;
+
+      
 //      XMI::Device::MPIOldmulticastModel<XMI::Device::MPIDevice<XMI::SysDep::MPISysDep>, XMI::Device::MPIMessage> _ccmi_mcast;
 //      XMI::Device::MPIOldmulticastModel<XMI::Device::MPIDevice<XMI::SysDep::MPISysDep>, XMI::Device::MPIMessage> _pgasrt_mcast;
 
