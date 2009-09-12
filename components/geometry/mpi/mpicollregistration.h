dfe434603b55f7a6cebebf3afb53c0573e308c5a
Charles Archer <archerc@CoreWhore.local>
2009-09-12 16:45:42 -0500

    adjust some collective registration code

--- components/geometry/mpi/mpicollregistration.h
+++ components/geometry/mpi/mpicollregistration.h
@@ -24,24 +24,29 @@ namespace XMI
           // Register and link each collective into a queue for analysis
 	  _nbColl.initialize();
           _nbColl.instance()->multisend_reg(TSPColl::BcastTag, &_pgbroadcast._model);
-	  //          _broadcasts.push_back(&_pgbroadcast);
+	  _broadcasts.push_back(&_pgbroadcast);
 
           _nbColl.instance()->multisend_reg(TSPColl::AllgatherTag, &_pgallgather._model);
-	  //          _allgathers.push_back(&_pgallgather);
+	  _allgathers.push_back(&_pgallgather);
+
           _nbColl.instance()->multisend_reg(TSPColl::AllgathervTag, &_pgallgatherv._model);
-	  //          _allgathervs.push_back(&_pgallgatherv);
+	  _allgathervs.push_back(&_pgallgatherv);
+
           _nbColl.instance()->multisend_reg(TSPColl::ScatterTag, &_pgscatter._smodel);
           _nbColl.instance()->multisend_reg(TSPColl::BarrierTag, &_pgscatter._bmodel);
-	  //          _scatters.push_back(&_pgscatter);
+	  _scatters.push_back(&_pgscatter);
+
           _nbColl.instance()->multisend_reg(TSPColl::ScattervTag, &_pgscatterv._smodel);
           _nbColl.instance()->multisend_reg(TSPColl::BarrierTag, &_pgscatterv._bmodel);
-	  //          _scattervs.push_back(&_pgscatterv);
-          _nbColl.instance()->multisend_reg(TSPColl::ShortAllreduceTag, &_pgallreduce._model);
-	  //          _allreduces.push_back(&_pgallreduce);
-          _nbColl.instance()->multisend_reg(TSPColl::BarrierTag, &_pgbarrier._model);
-	  //          _barriers.push_back(&_pgbarrier);
+	  _scattervs.push_back(&_pgscatterv);
 
-          return XMI_UNIMPL;
+          _nbColl.instance()->multisend_reg(TSPColl::ShortAllreduceTag, &_pgallreduce._model);
+	  _allreduces.push_back(&_pgallreduce);
+
+          _nbColl.instance()->multisend_reg(TSPColl::BarrierTag, &_pgbarrier._model);
+	  _barriers.push_back(&_pgbarrier);
+
+          return XMI_SUCCESS;
         }
       
       inline T_Collfactory * analyze_impl(T_Geometry *geometry)
@@ -50,14 +55,6 @@ namespace XMI
         }
 
     public:
-      RegQueue          _broadcasts;
-      RegQueue          _allgathers;
-      RegQueue          _allgathervs;
-      RegQueue          _scatters;
-      RegQueue          _scattervs;
-      RegQueue          _allreduces;
-      RegQueue          _barriers;
-      
       XMI_NBCollManager _nbColl;
       XMI::CollInfo::PGBroadcastInfo   _pgbroadcast;
       XMI::CollInfo::PGAllgatherInfo   _pgallgather;
@@ -66,6 +63,17 @@ namespace XMI
       XMI::CollInfo::PGScattervInfo    _pgscatterv;
       XMI::CollInfo::PGAllreduceInfo   _pgallreduce;
       XMI::CollInfo::PGBarrierInfo     _pgbarrier;
+
+
+      RegQueue          _broadcasts;
+      RegQueue          _allgathers;
+      RegQueue          _allgathervs;
+      RegQueue          _scatters;
+      RegQueue          _scattervs;
+      RegQueue          _allreduces;
+      RegQueue          _barriers;
+      
+
 
       MPIMcastModel     _ccmi_broadcast;      
       MPIMcastModel     _ccmi_allreduce;
