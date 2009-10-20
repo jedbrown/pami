3d419f822bf48be1c8d5c9921f61de3ee0539011
Douglas Miller <dougmill@us.ibm.com>
2009-10-20 10:21:54 -0500

    Issue 218: fix XL atomics for argument type change

--- compiler/xl/Compiler.h
+++ compiler/xl/Compiler.h
@@ -17,10 +17,10 @@
 // Here go things specific to this compiler family
 
 #include "builtins.h"
-#define __sync_fetch_and_or(x,y)	__fetch_and_or(x,y)
-#define __sync_fetch_and_and(x,y)	__fetch_and_and(x,y)
-#define __sync_fetch_and_add(x,y)	__fetch_and_add(x,y)
-#define __sync_fetch_and_sub(x,y)	__fetch_and_add(x,-(y))
-#define __sync_fetch_and_swap(x,y)	__fetch_and_swap(x,y)
+#define __sync_fetch_and_or(x,y)	__fetch_and_or((volatile int *)x,y)
+#define __sync_fetch_and_and(x,y)	__fetch_and_and((volatile int *)x,y)
+#define __sync_fetch_and_add(x,y)	__fetch_and_add((volatile int *)x,y)
+#define __sync_fetch_and_sub(x,y)	__fetch_and_add((volatile int *)x,-(y))
+#define __sync_fetch_and_swap(x,y)	__fetch_and_swap((volatile int *)x,y)
 
 #endif // __xmi_compiler_xl_h__
