4d7e5b271ec6ae36bc853f461ddb08fa70060b79
Douglas Miller <dougmill@us.ibm.com>
2009-10-26 11:46:46 -0500

    Issue 218: templatize multisync tests to use common test code

--- test/internals/multisend/bgp/multisync.cc
+++ test/internals/multisend/bgp/multisync.cc
@@ -1,27 +1,9 @@
 #include <stdio.h>
 #include "sys/xmi.h"
 
-#include "PipeWorkQueue.h"
-
-#include "Global.h"
 #include "components/devices/bgp/global_interrupt/GIBarrierMsg.h"
+#include "test/internals/multisend/multisync.h"
 
-
-void fail_reg(const char *s) {
-	fprintf(stderr, "failed to register \"%s\"\n", s);
-	exit(1);
-}
-
-char gib[sizeof(XMI::Device::BGP::giModel)];
-char gibm[XMI::Device::BGP::giModel::sizeof_msg];
-
-int done = 0;
-
-void done_cb(xmi_context_t context, void *cookie, xmi_result_t result) {
-	// printf skews timing too much...
-	//fprintf(stderr, "... completion callback for %s, done %d ++\n", (char *)cookie, done);
-	++done;
-}
 
 int main(int argc, char ** argv) {
 	unsigned x;
@@ -50,7 +32,7 @@ int main(int argc, char ** argv) {
 		return 1;
 	}
 	size_t task_id = configuration.value.intval;
-	fprintf(stderr, "My task id = %zd\n", task_id);
+	//fprintf(stderr, "My task id = %zd\n", task_id);
 
 	configuration.name = XMI_NUM_TASKS;
 	status = XMI_Configuration_query(context, &configuration);
@@ -59,75 +41,37 @@ int main(int argc, char ** argv) {
 		return 1;
 	}
 	size_t num_tasks = configuration.value.intval;
-	fprintf(stderr, "Number of tasks = %zd\n", num_tasks);
+	if (task_id == 0) fprintf(stderr, "Number of tasks = %zd\n", num_tasks);
+
+	if (__global.mapping.tSize() != 1) {
+		fprintf(stderr, "This test requires SMP mode\n");
+		exit(1);
+	}
 
 // END standard setup
 // ------------------------------------------------------------------------
 
-	// Do two barriers: first is to synchronize the ranks so that
-	// the timing of the second barrier is cleaner. Also, no printfs
-	// in the barriers because that messes up timing.  So, if the barrier
-	// needs debug then the printfs will have to be re-enabled.
-
-	// Basic timing profile: Each rank will delay prior to the start
-	// by the rank multiplied by 1000 (cycles). The idea being, if the
-	// barrier actually does it's job then the total time spent by each
-	// rank should be (relatively) close to the others. NOTE: these times
-	// are not to be used as performance numbers! They intentionally introduce
-	// extra time in order to show that the barrier is functional.
-
 	// Register some multisyncs, C++ style
-	unsigned long long t0, t1, t2;
-	bool rc;
-
-	const char *test = "giModel";
-	XMI::Device::BGP::giModel *model = new (&gib) XMI::Device::BGP::giModel(status);
-	if (status != XMI_SUCCESS) fail_reg(test);
+	xmi_result_t rc;
 
 	xmi_multisync_t msync;
 
-	// simple allreduce on the tree... SMP mode (todo: check and error)
-	msync.request = &gibm;
-	msync.cb_done = (xmi_callback_t){done_cb, (void *)test};
+	// simple barrier on the GI network... SMP mode
+	msync.context = context;
 	msync.roles = (unsigned)-1;
-	msync.participants = NULL;
-	fprintf(stderr, "... before %s.postMultisync\n", test);
-	done = 0;
-	rc = model->postMultisync(&msync);
-	if (!rc) {
-		fprintf(stderr, "Failed to post first multisync \"%s\"\n", test);
-		// need to skip rest...
-	}
-	// printf skews timing too much...
-	//fprintf(stderr, "... before advance loop for %s.postMultisync\n", test);
-	while (!done) {
-		status = XMI_Context_advance(context, 100);
-		if (status != XMI_SUCCESS) {
-			fprintf (stderr, "Error. Unable to advance xmi context. result = %d\n", status);
-			return 1;
-		}
-	}
-	t0 = __global.time.timebase();
-	while ((t1 = __global.time.timebase()) - t0 < task_id * 1000);
-	done = 0;
-	rc = model->postMultisync(&msync);
-	if (!rc) {
-		fprintf(stderr, "Failed to post second multisync \"%s\"\n", test);
-		// need to skip rest...
-	}
+	msync.participants = (xmi_topology_t *)&__global.topology_global;
 
-	// printf skews timing too much...
-	//fprintf(stderr, "... before advance loop for %s.postMultisync\n", test);
-	while (!done) {
-		status = XMI_Context_advance(context, 100);
-		// assert(status == XMI_SUCCESS);
+	const char *test = "XMI::Device::BGP::giModel";
+	if (task_id == 0) fprintf(stderr, "=== Testing %s...\n", test);
+	XMI::Test::Multisend::Multisync<XMI::Device::BGP::giModel> test1(test);
+	rc = test1.perform_test(task_id, num_tasks, &msync);
+	if (rc != XMI_SUCCESS) {
+		fprintf(stderr, "Failed %s test result = %d\n", test, rc);
+		exit(1);
 	}
-	t2 = __global.time.timebase();
-	// first number is total time for test barrier, second number is time for *this*
-	// rank's barrier. The second numbers should show a significant variation
-	// between ranks, while the first number should be more uniform.
-	fprintf(stderr, "PASS? %5lld (%5lld)\n", t2 - t0, t2 - t1);
+	fprintf(stderr, "PASS? %5lld (%5lld) [delay: %lld, time: %lld]\n", test1.total_time, test1.barrier_time, test1.delay, test1.raw_time);
 
+// ------------------------------------------------------------------------
 	status = XMI_Context_destroy(context);
 	if (status != XMI_SUCCESS) {
 		fprintf(stderr, "Error. Unable to destroy xmi context. result = %d\n", status);
--- test/internals/multisend/multisync.cc
+++ test/internals/multisend/multisync.cc
@@ -1,46 +1,24 @@
 #include <stdio.h>
 #include "sys/xmi.h"
 
-#include "Topology.h"
 #include "components/devices/generic/AtomicBarrierMsg.h"
-
-#include "Global.h"
-
-void fail_reg(const char *s) {
-	fprintf(stderr, "failed to register \"%s\"\n", s);
-	exit(1);
-}
-
-#ifdef NOTHING__bgp__
-
-#warning using BGP lockbox barrier to avoid bug in CounterBarrier/GccCounter
-#define BARRIER_NAME	"XMI::Barrier::BGP::LockBoxNodeProcBarrier"
-#include "components/atomic/bgp/LockBoxBarrier.h"
-typedef XMI::Barrier::BGP::LockBoxNodeProcBarrier<XMI_SYSDEP_CLASS> Barrier_Type;
-
-#else
+#include "test/internals/multisend/multisync.h"
 
 #define BARRIER_NAME	"XMI::Barrier::CounterBarrier<XMI::Counter::GccNodeCounter>"
 #include "components/atomic/gcc/GccCounter.h"
 #include "components/atomic/counter/CounterBarrier.h"
 typedef XMI::Barrier::CounterBarrier<XMI_SYSDEP_CLASS,XMI::Counter::GccNodeCounter<XMI_SYSDEP_CLASS> > Barrier_Type;
 
-#endif
-
 typedef XMI::Device::AtomicBarrierMdl<Barrier_Type> Barrier_Model;
 
-char ab[sizeof(Barrier_Model)];
-char abm[Barrier_Model::sizeof_msg];
+#ifdef __bgp__
 
-XMI_TOPOLOGY_CLASS topo;
+#define BARRIER_NAME2	"XMI::Barrier::BGP::LockBoxNodeProcBarrier"
+#include "components/atomic/bgp/LockBoxBarrier.h"
+typedef XMI::Barrier::BGP::LockBoxNodeProcBarrier<XMI_SYSDEP_CLASS> Barrier_Type2;
+typedef XMI::Device::AtomicBarrierMdl<Barrier_Type2> Barrier_Model2;
 
-int done = 0;
-
-void done_cb(xmi_context_t context, void *cookie, xmi_result_t result) {
-	// printf skews timing too much...
-	//fprintf(stderr, "... completion callback for %s, done %d ++\n", (char *)cookie, done);
-	++done;
-}
+#endif // __bgp__
 
 int main(int argc, char ** argv) {
 	unsigned x;
@@ -69,7 +47,7 @@ int main(int argc, char ** argv) {
 		return 1;
 	}
 	size_t task_id = configuration.value.intval;
-	fprintf(stderr, "My task id = %zd\n", task_id);
+	//fprintf(stderr, "My task id = %zd\n", task_id);
 
 	configuration.name = XMI_NUM_TASKS;
 	status = XMI_Configuration_query(context, &configuration);
@@ -78,7 +56,7 @@ int main(int argc, char ** argv) {
 		return 1;
 	}
 	size_t num_tasks = configuration.value.intval;
-	fprintf(stderr, "Number of tasks = %zd\n", num_tasks);
+	if (task_id == 0) fprintf(stderr, "Number of tasks = %zd\n", num_tasks);
 
 	if (__global.topology_local.size() < 2) {
 		fprintf(stderr, "This test requires more than one \"local\" rank.\n");
@@ -88,104 +66,49 @@ int main(int argc, char ** argv) {
 // END standard setup
 // ------------------------------------------------------------------------
 
-	// Do two barriers: first is to synchronize the ranks so that
-	// the timing of the second barrier is cleaner. Also, no printfs
-	// in the barriers because that messes up timing.  So, if the barrier
-	// needs debug then the printfs will have to be re-enabled.
-
-	// Basic timing profile: Each rank will delay prior to the start
-	// by the rank multiplied by 1000 (cycles). The idea being, if the
-	// barrier actually does it's job then the total time spent by each
-	// rank should be (relatively) close to the others. NOTE: these times
-	// are not to be used as performance numbers! They intentionally introduce
-	// extra time in order to show that the barrier is functional.
-
 	// Register some multisyncs, C++ style
-	unsigned long long t0, t1, t2, delay, raw;
-	bool rc;
-
-	const char *test = BARRIER_NAME;
-	Barrier_Model *model = new (&ab) Barrier_Model(status);
-	if (status != XMI_SUCCESS) fail_reg(test);
-
-	new (&topo) XMI_TOPOLOGY_CLASS(task_id);
+	xmi_result_t rc;
 
 	xmi_multisync_t msync;
 
 	// simple allreduce on the tree... SMP mode (todo: check and error)
-	msync.request = &abm;
-	msync.cb_done = (xmi_callback_t){done_cb, (void *)test};
+	msync.context = context;
 	msync.roles = (unsigned)-1;
-	msync.participants = (xmi_topology_t *)&topo;
-	fprintf(stderr, "... before %s.postMultisync\n", test);
-	done = 0;
-	rc = model->postMultisync(&msync);
-	if (!rc) {
-		fprintf(stderr, "Failed to post first multisync \"%s\"\n", test);
-		// need to skip rest...
-	}
-	// printf skews timing too much...
-	//fprintf(stderr, "... before advance loop for %s.postMultisync\n", test);
-	while (!done) {
-		status = XMI_Context_advance(context, 100);
-		if (status != XMI_SUCCESS) {
-			fprintf (stderr, "Error. Unable to advance xmi context. result = %d\n", status);
-			return 1;
-		}
-	}
-	done = 0;
-	t0 = __global.time.timebase();
-	rc = model->postMultisync(&msync);
-	if (!rc) {
-		fprintf(stderr, "Failed to post second multisync \"%s\"\n", test);
-		// need to skip rest...
-	}
-	while (!done) {
-		status = XMI_Context_advance(context, 100);
-		// assert(status == XMI_SUCCESS);
-	}
-	raw = __global.time.timebase() - t0;
-	delay = raw * task_id;
-	//
-	// In order to get meaningful results below, we need to vary each rank's
-	// arrival time at the barrier by a "significant" amount. We use the
-	// time it took for the initial barrier, multiplied by our task ID, to
-	// create noticable differences in arrival time (we hope).
-	//
-	// The resulting timings should show that barrier times (in parens)
-	// vary a lot while the overall times should be more closely aligned.
-	//
-	// +--------------------------------------------------------------+
-	// |                                        |    barrier          |
-	// +--------------------------------------------------------------+
-	// |                         |    barrier                         |
-	// +--------------------------------------------------------------+
-	// |               |    barrier                                   |
-	// +--------------------------------------------------------------+
-	// |                                             |    barrier     |
-	// +--------------------------------------------------------------+
-	// t0            (t1[x] ...         ...      ... )                t2
-	//
-	t0 = __global.time.timebase();
-	while ((t1 = __global.time.timebase()) - t0 < delay);
-	done = 0;
-	rc = model->postMultisync(&msync);
-	if (!rc) {
-		fprintf(stderr, "Failed to post third multisync \"%s\"\n", test);
-		// need to skip rest...
+	msync.participants = (xmi_topology_t *)&__global.topology_local;
+
+	const char *test = BARRIER_NAME;
+	if (task_id == 0) fprintf(stderr, "=== Testing %s...\n", test);
+	XMI::Test::Multisend::Multisync<Barrier_Model> test1(test);
+	rc = test1.perform_test(task_id, num_tasks, &msync);
+	if (rc != XMI_SUCCESS) {
+		fprintf(stderr, "Failed %s test result = %d\n", test, rc);
+		exit(1);
 	}
 
-	// printf skews timing too much...
-	//fprintf(stderr, "... before advance loop for %s.postMultisync\n", test);
-	while (!done) {
-		status = XMI_Context_advance(context, 100);
-		// assert(status == XMI_SUCCESS);
-	}
-	t2 = __global.time.timebase();
 	// first number is total time for test barrier, second number is time for *this*
 	// rank's barrier. The second numbers should show a significant variation
 	// between ranks, while the first number should be more uniform.
-	fprintf(stderr, "PASS? %5lld (%5lld) [delay: %lld, time: %lld]\n", t2 - t0, t2 - t1, delay, raw);
+	fprintf(stderr, "PASS? %5lld (%5lld) [delay: %lld, time: %lld]\n", test1.total_time, test1.barrier_time, test1.delay, test1.raw_time);
+
+#ifdef __bgp__
+
+	test = BARRIER_NAME2;
+	if (task_id == 0) fprintf(stderr, "=== Testing %s...\n", test);
+	XMI::Test::Multisend::Multisync<Barrier_Model2> test2(test);
+	rc = test2.perform_test(task_id, num_tasks, &msync);
+	if (rc != XMI_SUCCESS) {
+		fprintf(stderr, "Failed %s test result = %d\n", test, rc);
+		exit(1);
+	}
+
+	// first number is total time for test barrier, second number is time for *this*
+	// rank's barrier. The second numbers should show a significant variation
+	// between ranks, while the first number should be more uniform.
+	fprintf(stderr, "PASS2? %5lld (%5lld) [delay: %lld, time: %lld]\n", test2.total_time, test2.barrier_time, test2.delay, test2.raw_time);
+
+#endif // __bgp__
+
+// ------------------------------------------------------------------------
 
 	status = XMI_Context_destroy(context);
 	if (status != XMI_SUCCESS) {
