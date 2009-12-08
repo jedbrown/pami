5ba49920b11b33a5f8f3e1941b98e51088d3cede
Sameer Kumar <sameerk@us.ibm.com>
2009-12-08 09:59:46 -0600

    Calling dispatch_set_new from native interface.

--- components/geometry/mpi/MPINativeInterface.h
+++ components/geometry/mpi/MPINativeInterface.h
@@ -25,13 +25,15 @@ namespace XMI {
   template <class T_Device>
   class MPINativeInterface : public CCMI::Interfaces::NativeInterface {
 
+#if 0
   typedef XMI::Device::MPIMessage MPIMessage;
   typedef XMI::Device::MPIPacketModel<T_Device,MPIMessage> MPIPacketModel;
   typedef XMI::Protocol::Send::Eager <MPIPacketModel,T_Device> EagerMPI;
   typedef XMI::Protocol::MPI::P2pDispatchMulticastProtocol<T_Device,EagerMPI,XMI::Device::MPIBcastMdl> P2pDispatchMulticastProtocol;
+#endif
 
   public:
-  MPINativeInterface(T_Device *dev, xmi_client_t client, xmi_context_t context, size_t context_id): CCMI::Interfaces::NativeInterface(__global.mapping.task(), __global.mapping.size()), _device(dev), _protocol(_status), _dispatch(0), _client(client), _context(context), _contextid(context_id) {}
+  MPINativeInterface(T_Device *dev, xmi_client_t client, xmi_context_t context, size_t context_id): CCMI::Interfaces::NativeInterface(__global.mapping.task(), __global.mapping.size()), _device(dev), /*_protocol(_status),*/ _dispatch(0), _client(client), _context(context), _contextid(context_id) {}
   
     /// \brief this call is called when the native interface is initialized
     virtual void setDispatch (xmi_dispatch_callback_fn fn, void *cookie) {
@@ -39,8 +41,17 @@ namespace XMI {
 
       printf ("In set dispatch, id = %d\n", dispatch);
 
-      new (&_protocol) P2pDispatchMulticastProtocol(dispatch, fn.multicast, cookie, *_device, _client, _context, _contextid, _status);
-      //_protocol.registerMcastRecvFunction (dispatch, fn.multicast, cookie);
+      /*new (&_protocol) P2pDispatchMulticastProtocol(dispatch, fn.multicast, cookie, *_device, _client, _context, _contextid, _status);
+      //_protocol.registerMcastRecvFunction (dispatch, fn.multicast, cookie);*/
+
+      xmi_dispatch_hint_t        options;
+      memset(&options, 0x00, sizeof(options));
+      options.type = XMI_MULTICAST;
+      options.config = NULL;
+      options.hint.multicast.global = 1;
+      options.hint.multicast.one_sided = 1;
+      options.hint.multicast.active_message = 1;      
+      XMI_Dispatch_set_new (_context, dispatch, fn, cookie, options);
       
       CCMI_assert (_status == XMI_SUCCESS);
       _dispatch = dispatch;
@@ -59,7 +70,7 @@ namespace XMI {
   private:
     T_Device                 *_device;
 
-    P2pDispatchMulticastProtocol      _protocol;
+    //    P2pDispatchMulticastProtocol      _protocol;
     xmi_result_t                      _status;
     unsigned                          _dispatch;
     xmi_client_t                      _client;
