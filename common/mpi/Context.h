f2c67921dbf0e8bbb5f1274084b02c9c03275a7d
Charles Archer <archerc@us.ibm.com>
2010-03-02 09:53:36 -0600

    Removed extra adaptive include from mpi context

--- common/mpi/Context.h
+++ common/mpi/Context.h
@@ -17,7 +17,6 @@
 #include "components/devices/mpi/mpipacketmodel.h"
 #include "components/devices/mpi/mpimessage.h"
 
-#include "p2p/protocols/send/adaptive/Adaptive.h"
 #include "p2p/protocols/send/eager/Eager.h"
 #include "p2p/protocols/send/eager/EagerSimple.h"
 #include "p2p/protocols/send/eager/EagerImmediate.h"
