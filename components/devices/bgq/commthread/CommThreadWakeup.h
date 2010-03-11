be7ea3682a9af15135fce776c16368c9d3a7a46a
Douglas Miller <dougmill@us.ibm.com>
2010-03-11 10:06:09 -0600

    Trac #70: initial stub for prototyping BGQ commthread code

---
+++ components/devices/bgq/commthread/CommThreadWakeup.h
@@ -0,0 +1,24 @@
+/* begin_generated_IBM_copyright_prolog                             */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/* (C)Copyright IBM Corp.  2009, 2010                               */
+/* IBM CPL License                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* end_generated_IBM_copyright_prolog                               */
+
+/// \file components/devices/bgq/commthread/CommThread.h
+/// \brief
+
+#ifndef __components_devices_bgq_commthread_CommThread_h__
+#define __components_devices_bgq_commthread_CommThread_h__
+
+namespace XMI {
+namespace Device {
+namespace CommThread {
+
+}; // namespace CommThread
+}; // namespace Device
+}; // namespace XMI
+
+#endif // __components_devices_bgq_commthread_CommThread_h__
