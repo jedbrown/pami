8dce6cc52d733679dd156e74620cdebd0059789e
Douglas Miller <dougmill@us.ibm.com>
2010-03-11 07:50:27 -0600

    Trac #69: initial clone of L2 atomics objects

---
+++ components/atomic/l2/L2Barrier.h
@@ -0,0 +1,174 @@
+/* begin_generated_IBM_copyright_prolog                             */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/* (C)Copyright IBM Corp.  2009, 2010                               */
+/* IBM CPL License                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* end_generated_IBM_copyright_prolog                               */
+
+#ifndef __components_atomic_bgp_LockBoxBarrier_h__
+#define __components_atomic_bgp_LockBoxBarrier_h__
+
+/**
+ * \file components/atomic/bgp/LockBoxBarrier.h
+ * \brief LockBox implementation of a Node-scoped Barrier
+ *
+ * This implementation is different from, and incompatible with, the
+ * BGP SPI LockBox_Barrier implementation. Support for core
+ * granularity barriers is added here, in a way that makes the barrier
+ * code common for all granularities.
+ */
+#include "common/bgp/LockBoxFactory.h"
+#include "components/atomic/Barrier.h"
+#include "Global.h"
+#include <spi/bgp_SPI.h>
+
+namespace XMI {
+namespace Barrier {
+namespace BGP {
+/*
+ * This class cannot be used directly. The super class must allocate the
+ * particular type of lockbox based on desired scope.
+ */
+class _LockBoxBarrier {
+private:
+	/**
+	 * \brief Base structure for lockbox barrier
+	 *
+	 * This houses 5 lockboxes which are used to implement
+	 * a barrier.
+	 */
+	struct LockBox_Barrier_s {
+		union {
+			uint32_t *lkboxes[5]; /**< access all 5 lockboxes */
+			struct {
+				LockBox_Counter_t ctrl_lock; /**< the control lockbox (phase) */
+				LockBox_Counter_t lock[2]; /**< lock lockboxes for all phases */
+				LockBox_Counter_t status[2]; /**< status lockboxes for all phases */
+			} lbx_s;
+		} lbx_u;
+		uint8_t master;    /**< master participant */
+		uint8_t coreshift; /**< convert core to process for comparing to master */
+		uint8_t nparties;  /**< number of participants */
+		uint8_t _pad;      /**< pad to int */
+	};
+#define lbx_lkboxes	lbx_u.lkboxes		/**< shortcut for lkboxes */
+#define lbx_ctrl_lock	lbx_u.lbx_s.ctrl_lock	/**< shortcut for ctrl_lock */
+#define lbx_lock	lbx_u.lbx_s.lock	/**< shortcut for lock */
+#define lbx_status	lbx_u.lbx_s.status	/**< shortcut for status */
+public:
+	_LockBoxBarrier() { }
+	~_LockBoxBarrier() { }
+
+	inline void init_impl(void *sd, size_t z, bool m) {
+		XMI_abortf("_LockBoxBarrier class must be subclass");
+	}
+
+	inline xmi_result_t enter_impl() {
+		pollInit_impl();
+		while (poll_impl() != XMI::Atomic::Interface::Done);
+		return XMI_SUCCESS;
+	}
+
+	inline void enterPoll_impl(XMI::Atomic::Interface::pollFcn fcn, void *arg) {
+		pollInit_impl();
+		while (poll_impl() != XMI::Atomic::Interface::Done) {
+			fcn(arg);
+		}
+	}
+
+	inline void pollInit_impl() {
+		uint32_t lockup;
+		_bgp_msync();
+		lockup = LockBox_Query(_barrier.lbx_ctrl_lock);
+		LockBox_FetchAndInc(_barrier.lbx_lock[lockup]);
+		_data = (void*)lockup;
+		_status = XMI::Atomic::Interface::Entered;
+	}
+
+	inline XMI::Atomic::Interface::barrierPollStatus poll_impl() {
+		XMI_assert(_status == XMI::Atomic::Interface::Entered);
+		uint32_t lockup, value;
+		lockup = (unsigned)_data;
+		if (LockBox_Query(_barrier.lbx_lock[lockup]) < _barrier.nparties) {
+			return XMI::Atomic::Interface::Entered;
+		}
+
+		// All cores have participated in the barrier
+		// We need all cores to block until checkin
+		// to clear the lock atomically
+		LockBox_FetchAndInc(_barrier.lbx_lock[lockup]);
+		do {
+			value = LockBox_Query(_barrier.lbx_lock[lockup]);
+		} while (value > 0 && value < (unsigned)(2 * _barrier.nparties));
+
+		if ((Kernel_PhysicalProcessorID() >> _barrier.coreshift) == _barrier.master) {
+			if (lockup) {
+				LockBox_FetchAndDec(_barrier.lbx_ctrl_lock);
+			} else {
+				LockBox_FetchAndInc(_barrier.lbx_ctrl_lock);
+			}
+			LockBox_FetchAndClear(_barrier.lbx_status[lockup]);
+			LockBox_FetchAndClear(_barrier.lbx_lock[lockup]);
+		} else {
+			// wait until master releases the barrier by clearing the lock
+			while (LockBox_Query(_barrier.lbx_lock[lockup]) > 0);
+		}
+		_status = XMI::Atomic::Interface::Initialized;
+		return XMI::Atomic::Interface::Done;
+	}
+	// With 5 lockboxes used... which one should be returned?
+	inline void *returnBarrier_impl() { return _barrier.lbx_ctrl_lock; }
+protected:
+	LockBox_Barrier_s _barrier;
+	void *_data;
+	XMI::Atomic::Interface::barrierPollStatus _status;
+}; // class _LockBoxBarrier
+
+class LockBoxNodeCoreBarrier :
+		public XMI::Atomic::Interface::Barrier<LockBoxNodeCoreBarrier>,
+		public _LockBoxBarrier {
+public:
+	LockBoxNodeCoreBarrier() {}
+	~LockBoxNodeCoreBarrier() {}
+	inline void init_impl(XMI::SysDep *sd, size_t z, bool m) {
+		// For core-granularity, everything is
+		// a core number. Assume the master core
+		// is the lowest-numbered core in the
+		// process.
+#warning take master cue from params?
+		_barrier.master = __global.lockboxFactory.masterProc() << __global.lockboxFactory.coreShift();
+		_barrier.coreshift = 0;
+		_barrier.nparties = __global.lockboxFactory.numCore();
+		__global.lockboxFactory.lbx_alloc((void **)_barrier.lbx_lkboxes, 5,
+						XMI::Atomic::BGP::LBX_NODE_SCOPE);
+		_status = XMI::Atomic::Interface::Initialized;
+	}
+}; // class LockBoxNodeCoreBarrier
+
+class LockBoxNodeProcBarrier :
+		public XMI::Atomic::Interface::Barrier<LockBoxNodeProcBarrier>,
+		public _LockBoxBarrier {
+public:
+	LockBoxNodeProcBarrier() {}
+	~LockBoxNodeProcBarrier() {}
+	inline void init_impl(XMI::SysDep *sd, size_t z, bool m) {
+		// For proc-granularity, must convert
+		// between core id and process id,
+		// and only one core per process will
+		// participate.
+		_barrier.master = __global.lockboxFactory.coreXlat(__global.lockboxFactory.masterProc()) >> __global.lockboxFactory.coreShift();
+		_barrier.coreshift = __global.lockboxFactory.coreShift();
+		_barrier.nparties = __global.lockboxFactory.numProc();
+		__global.lockboxFactory.lbx_alloc((void **)_barrier.lbx_lkboxes, 5,
+						XMI::Atomic::BGP::LBX_NODE_SCOPE);
+		_status = XMI::Atomic::Interface::Initialized;
+	}
+}; // class LockBoxNodeProcBarrier
+
+}; // BGP namespace
+}; // Barrier namespace
+}; // XMI namespace
+
+#endif // __xmi_bgp_lockboxbarrier_h__
---
+++ components/atomic/l2/L2Mutex.h
@@ -0,0 +1,161 @@
+/* begin_generated_IBM_copyright_prolog                             */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/* (C)Copyright IBM Corp.  2009, 2010                               */
+/* IBM CPL License                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* end_generated_IBM_copyright_prolog                               */
+
+#ifndef __components_atomic_bgp_LockBoxMutex_h__
+#define __components_atomic_bgp_LockBoxMutex_h__
+
+/**
+ * \file components/atomic/bgp/LockBoxMutex.h
+ * \brief BGP implementation of Node- or Process-scoped Mutexes
+ *
+ * LockBox is the only implementation, currently. Additional implementations
+ * would be new classes, all inheriting from "Mutex".
+ *
+ *
+ */
+#include "Global.h"
+#include "SysDep.h"
+#include "common/bgp/LockBoxFactory.h"
+#include "components/atomic/Mutex.h"
+#include <spi/bgp_SPI.h>
+#include <bpcore/bgp_atomic_ops.h>
+
+#ifndef __defined__tsc__
+#define __defined__tsc__
+static inline unsigned long __tsc() {
+	unsigned long tsc;
+	asm volatile ("mfspr %0,%1" : "=r" (tsc) : "i" (SPRN_TBRL));
+	return tsc;
+}
+#endif /* ! __defined__tsc__ */
+
+namespace XMI {
+namespace Mutex {
+	//
+	// These classes are used internally ONLY. See following classes for users
+	//
+	class _LockBoxMutex {
+	public:
+		_LockBoxMutex() { _addr = NULL; }
+		inline void init_impl(XMI::SysDep *sd) {
+			XMI_abortf("_LockBoxMutex must be a subclass");
+		}
+		void acquire_impl() {
+			LockBox_MutexLock((LockBox_Mutex_t)_addr);
+		}
+		void release_impl() {
+			LockBox_MutexUnlock((LockBox_Mutex_t)_addr);
+		}
+		bool tryAcquire_impl() {
+			return (LockBox_MutexTryLock((LockBox_Mutex_t)_addr) == 0) ? true : false;
+		}
+		bool isLocked_impl() {
+			return (LockBox_Query((LockBox_Counter_t)_addr) > 0) ? true : false;
+		}
+		void *returnLock_impl() { return _addr; }
+	protected:
+		void *_addr;
+	}; // class _LockBoxMutex
+
+	class _FairLockBoxMutex {
+	public:
+		_FairLockBoxMutex() { _addr = NULL; }
+		inline void init_impl(XMI::SysDep *sd) {
+			XMI_abortf("_FairLockBoxMutex must be a subclass");
+		}
+		void acquire_impl() {
+			// LockBox_MutexLock((LockBox_Mutex_t)_addr);
+			//
+			// There is some unknown problem with lockbox mutexes whereby
+			// some contenders get "shut-out" and effectively never get
+			// the mutex. The following back-off scheme seems to eliminate
+			// the problem. If we don't get the lock on the first try,
+			// we delay a short time to keep from swamping the lockbox.
+			// We continue trying and delaying, however if we detect that
+			// someone else got the lock instead of us (we see the lock value
+			// decrease - or not increase), then we reduce our delay amount.
+			// This means that if we wait too long, we effectively have no
+			// delay, while other contenders that got the lock recently
+			// will be delaying by the full amount and thus be at a greater
+			// disadvantage compared to us. This imbalance of traffic seems
+			// to be enough to "break the spell" and allow us to get the lock.
+			//
+			unsigned long lbv, lst = 0;
+			unsigned long td = 127;
+			while ((lbv = LockBox_FetchAndInc((LockBox_Counter_t)_addr)) != 0) {
+				if (td && lbv <= lst) --td;
+				lst = lbv;
+				if (td) {
+					unsigned long t0 = __tsc();
+					while (__tsc() - t0 < td);
+				}
+			}
+		}
+		void release_impl() {
+			LockBox_MutexUnlock((LockBox_Mutex_t)_addr);
+		}
+		bool tryAcquire_impl() {
+			return (LockBox_MutexTryLock((LockBox_Mutex_t)_addr) == 0) ? true : false;
+		}
+		bool isLocked_impl() {
+			return (LockBox_Query((LockBox_Counter_t)_addr) > 0) ? true : false;
+		}
+		void *returnLock_impl() { return _addr; }
+	protected:
+		void *_addr;
+	}; // class _FairLockBoxMutex
+
+	//
+	// Here are the actual classes to be used:
+	//
+
+	class LockBoxNodeMutex : public _LockBoxMutex,
+				 public XMI::Atomic::Interface::Mutex<LockBoxNodeMutex> {
+	public:
+		LockBoxNodeMutex() {}
+		~LockBoxNodeMutex() {}
+		inline void init_impl(XMI::SysDep *sd) {
+			__global.lockboxFactory.lbx_alloc(&this->_addr, 1, XMI::Atomic::BGP::LBX_NODE_SCOPE);
+		}
+	}; // class LockBoxNodeMutex
+
+	class LockBoxProcMutex : public _LockBoxMutex,
+				 public XMI::Atomic::Interface::Mutex<LockBoxProcMutex> {
+	public:
+		LockBoxProcMutex() {}
+		~LockBoxProcMutex() {}
+		inline void init_impl(XMI::SysDep *sd) {
+			__global.lockboxFactory.lbx_alloc(&this->_addr, 1, XMI::Atomic::BGP::LBX_PROC_SCOPE);
+		}
+	}; // class LockBoxProcMutex
+
+	class FairLockBoxNodeMutex : public _FairLockBoxMutex,
+				     public XMI::Atomic::Interface::Mutex<FairLockBoxNodeMutex> {
+	public:
+		FairLockBoxNodeMutex() {}
+		~FairLockBoxNodeMutex() {}
+		inline void init_impl(XMI::SysDep *sd) {
+			__global.lockboxFactory.lbx_alloc(&this->_addr, 1, XMI::Atomic::BGP::LBX_NODE_SCOPE);
+		}
+	}; // class FairLockBoxNodeMutex
+
+	class FairLockBoxProcMutex : public _FairLockBoxMutex,
+				     public XMI::Atomic::Interface::Mutex<FairLockBoxProcMutex> {
+	public:
+		FairLockBoxProcMutex() {}
+		~FairLockBoxProcMutex() {}
+		inline void init_impl(XMI::SysDep *sd) {
+			__global.lockboxFactory.lbx_alloc(&this->_addr, 1, XMI::Atomic::BGP::LBX_PROC_SCOPE);
+		}
+	}; // class FairLockBoxProcMutex
+
+}; // Mutex namespace
+}; // XMI namespace
+
+#endif // __components_atomic_bgp_lockboxmutex_h__
