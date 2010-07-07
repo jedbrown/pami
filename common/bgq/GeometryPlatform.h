0f4a5a0b3c3351c30477f02c4d345008a43115ab
Douglas Miller <dougmill@us.ibm.com>
2010-07-07 12:58:01 -0500

    Trac #120: oops, forgot new files

---
+++ common/bgq/GeometryPlatform.h
@@ -0,0 +1,20 @@
+/* begin_generated_IBM_copyright_prolog                             */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/* (C)Copyright IBM Corp.  2009, 2010                               */
+/* IBM CPL License                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* end_generated_IBM_copyright_prolog                               */
+
+/// \file common/bgq/GeometryPlatform.h
+/// BG/Q platform extensions to Geometry class
+
+#ifndef __common_bgq_GeometryPlatform_h__
+#define __common_bgq_GeometryPlatform_h__
+
+/// \brief Extensions to enum keys_t
+#define PAMI_GKEY_PLATEXTENSIONS       \
+       PAMI_GKEY_CLASSROUTE,
+
+#endif // __common_bgq_GeometryPlatform_h__
---
+++ common/default/GeometryPlatform.h
@@ -0,0 +1,19 @@
+/* begin_generated_IBM_copyright_prolog                             */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/* (C)Copyright IBM Corp.  2009, 2010                               */
+/* IBM CPL License                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* end_generated_IBM_copyright_prolog                               */
+
+/// \file common/default/GeometryPlatform.h
+/// Default (empty) platform extensions to Geometry class
+
+#ifndef __common_default_GeometryPlatform_h__
+#define __common_default_GeometryPlatform_h__
+
+/// \brief Extensions to enum keys_t
+#define PAMI_GKEY_PLATEXTENSIONS
+
+#endif // __common_default_GeometryPlatform_h__
