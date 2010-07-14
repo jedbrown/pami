67fc78a7de9c4a0dd438943b15d6e7c978fd24cf
Douglas Miller <dougmill@us.ibm.com>
2010-07-14 09:21:09 -0500

    Trac #203: add runtime sanity check for commthreads tests

--- test/internals/BG/bgq/commthreads/multi-proc.c
+++ test/internals/BG/bgq/commthreads/multi-proc.c
@@ -11,100 +11,104 @@
 #include "commthread_test.h"
 
 int main(int argc, char ** argv) {
-  pami_client_t client;
-  char cl_string[] = "TEST";
-  pami_result_t result = PAMI_ERROR;
-  pami_context_t context[NUM_CONTEXTS];
-  size_t num_contexts;
-  int x, y;
-  size_t meix = TEST_Local_myindex();
-  char buf[64];
+	pami_client_t client;
+	char cl_string[] = "TEST";
+	pami_result_t result = PAMI_ERROR;
+	pami_context_t context[NUM_CONTEXTS];
+	size_t num_contexts;
+	int x, y;
+	size_t meix = TEST_Local_myindex();
+	char buf[64];
 
-  result = PAMI_Client_create(cl_string, &client);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to initialize pami client. "
-            "result = %d\n", result);
-    return 1;
-  }
+	result = PAMI_Client_create(cl_string, &client);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to initialize pami client. "
+						"result = %d\n", result);
+		return 1;
+	}
 
-  pami_configuration_t configuration;
-  configuration.name = PAMI_TASK_ID;
-  result = PAMI_Configuration_query(client, &configuration);
-  pami_task_t task = configuration.value.intval;
+	pami_configuration_t configuration;
+	configuration.name = PAMI_TASK_ID;
+	result = PAMI_Configuration_query(client, &configuration);
+	pami_task_t task = configuration.value.intval;
 
-  configuration.name = PAMI_NUM_TASKS;
-  result = PAMI_Configuration_query(client, &configuration);
-  size_t ntasks = configuration.value.intval;
-  sprintf(buf, "St %ld %d %zd\n", pthread_self(), task, ntasks);
-  int bufl = strlen(buf);
+	configuration.name = PAMI_NUM_TASKS;
+	result = PAMI_Configuration_query(client, &configuration);
+	size_t ntasks = configuration.value.intval;
+	sprintf(buf, "St %ld %d %zd\n", pthread_self(), task, ntasks);
+	if (ntasks & 1) {
+		fprintf(stderr, "Must be run with an even number of tasks\n");
+		exit(1);
+	}
+	int bufl = strlen(buf);
 
-  num_contexts = TEST_Local_size();
-  if (num_contexts > NUM_CONTEXTS) num_contexts = NUM_CONTEXTS;
-  result = PAMI_Context_createv(client, NULL, 0, &context[0], num_contexts);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to create %zd pami context. "
-            "result = %d\n", num_contexts, result);
-    return 1;
-  }
+	num_contexts = TEST_Local_size();
+	if (num_contexts > NUM_CONTEXTS) num_contexts = NUM_CONTEXTS;
+	result = PAMI_Context_createv(client, NULL, 0, &context[0], num_contexts);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to create %zd pami context. "
+						"result = %d\n", num_contexts, result);
+		return 1;
+	}
 
-  result = init_test_send(client, context, num_contexts);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to init test %zd pami contexts. "
-            "result = %d\n", num_contexts, result);
-    return 1;
-  }
+	result = init_test_send(client, context, num_contexts);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to init test %zd pami contexts. "
+						"result = %d\n", num_contexts, result);
+		return 1;
+	}
 
-  write(2, buf, bufl);
-  for (x = 0; x < num_contexts; ++x) {
-    result = PAMI_Client_add_commthread_context(client, context[x]);
-    if (result != PAMI_SUCCESS) {
-      fprintf(stderr, "Error. Unable to add commthread to context[%d]. "
-              "result = %d (%d)\n", x, result, errno);
-      return 1;
-    }
-  }
-  for (y = 0; y < NUM_TESTRUNS; ++y) {
-    for (x = 0; x < num_contexts; ++x) {
-      _info[x].seq = (task * 1000) + y * num_contexts + x + 1;
-    }
+	write(2, buf, bufl);
+	for (x = 0; x < num_contexts; ++x) {
+		result = PAMI_Client_add_commthread_context(client, context[x]);
+		if (result != PAMI_SUCCESS) {
+			fprintf(stderr, "Error. Unable to add commthread to context[%d]. "
+							"result = %d (%d)\n", x, result, errno);
+			return 1;
+		}
+	}
+	for (y = 0; y < NUM_TESTRUNS; ++y) {
+		for (x = 0; x < num_contexts; ++x) {
+			_info[x].seq = (task * 1000) + y * num_contexts + x + 1;
+		}
 
-    result = run_test_send(client, context, num_contexts, (meix ^ y) & 1);
-    if (result != PAMI_SUCCESS) {
-      fprintf(stderr, "Error. Unable to run commthread test. "
-              "result = %d\n", result);
-      return 1;
-    }
+		result = run_test_send(client, context, num_contexts, (meix ^ y) & 1);
+		if (result != PAMI_SUCCESS) {
+			fprintf(stderr, "Error. Unable to run commthread test. "
+							"result = %d\n", result);
+			return 1;
+		}
 
-    if (y + 1 < NUM_TESTRUNS) {
-      buf[0] = 'S'; buf[1] = 'p';
-      write(2, buf, bufl);
-      fprintf(stderr, "Sleeping...\n");
-      /* sleep(5); */
-      unsigned long long t0 = PAMI_Wtimebase();
-      //fprintf(stderr, "Woke up after %lld cycles\n", (PAMI_Wtimebase() - t0));
-      while (PAMI_Wtimebase() - t0 < 500000);
-      buf[0] = 'W'; buf[1] = 'a';
-      write(2, buf, bufl);
-    }
-  }
+		if (y + 1 < NUM_TESTRUNS) {
+			buf[0] = 'S'; buf[1] = 'p';
+			write(2, buf, bufl);
+			fprintf(stderr, "Sleeping...\n");
+			/* sleep(5); */
+			unsigned long long t0 = PAMI_Wtimebase();
+			//fprintf(stderr, "Woke up after %lld cycles\n", (PAMI_Wtimebase() - t0));
+			while (PAMI_Wtimebase() - t0 < 500000);
+			buf[0] = 'W'; buf[1] = 'a';
+			write(2, buf, bufl);
+		}
+	}
 
-  buf[0] = 'S'; buf[1] = 'p';
-  write(2, buf, bufl);
-  {unsigned long long t0 = PAMI_Wtimebase();
-    while (PAMI_Wtimebase() - t0 < 500000);}
-  buf[0] = 'F'; buf[1] = 'i';
-  write(2, buf, bufl);
-  result = PAMI_Context_destroyv(context, num_contexts);
-  result = PAMI_Client_destroy(&client);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to finalize pami client. "
-            "result = %d\n", result);
-    return 1;
-  }
+	buf[0] = 'S'; buf[1] = 'p';
+	write(2, buf, bufl);
+	{unsigned long long t0 = PAMI_Wtimebase();
+		while (PAMI_Wtimebase() - t0 < 500000);}
+	buf[0] = 'F'; buf[1] = 'i';
+	write(2, buf, bufl);
+	result = PAMI_Context_destroyv(context, num_contexts);
+	result = PAMI_Client_destroy(&client);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to finalize pami client. "
+						"result = %d\n", result);
+		return 1;
+	}
 
-  buf[0] = 'S'; buf[1] = 'u';
-  write(2, buf, bufl);
-  fprintf(stderr, "Success.\n");
+	buf[0] = 'S'; buf[1] = 'u';
+	write(2, buf, bufl);
+	fprintf(stderr, "Success.\n");
 
-  return 0;
+	return 0;
 }
--- test/internals/BG/bgq/commthreads/preempt-pthreads.c
+++ test/internals/BG/bgq/commthreads/preempt-pthreads.c
@@ -7,174 +7,181 @@
 
 int run = 0;
 struct thread_data {
-  pami_context_t context;
-  pthread_mutex_t mutex;
-  pthread_t thread;
-  volatile int active;
+	pami_context_t context;
+	pthread_mutex_t mutex;
+	pthread_t thread;
+	volatile int active;
 };
 
 void *user_pthread(void *cookie) {
-  struct thread_data *dat = (struct thread_data *)cookie;
-  pami_result_t rc;
-  int lrc;
-  int last = 0;
-  char buf[64];
-  sprintf(buf, "Ra %ld\n", pthread_self());
-  int bufl = strlen(buf);
-  write(2, buf, bufl);
-  while (run) {
-    while (run && dat->active) {
-      if (last == 0) {
-        buf[0] = 'T'; buf[1] = 'o';
-        write(2, buf, bufl);
-        last = 1;
-      }
-      rc = PAMI_Context_trylock(dat->context); // should cause wakeups...
-      if (rc == PAMI_SUCCESS) {
-        while (run && dat->active) {
-          // why doesn't advance return "num events" anymore?
-          rc = PAMI_Context_advance(dat->context, 100);
-        }
-        rc = PAMI_Context_unlock(dat->context);
-        // might as well just 'break' here...
-      }
-    }
-    last = 0;
-    buf[0] = 'G'; buf[1] = 'b';
-    write(2, buf, bufl);
-    lrc = pthread_mutex_lock(&dat->mutex);
-    // now, active == 1 || run == 0
-    pthread_mutex_unlock(&dat->mutex);
-  }
-  buf[0] = 'A'; buf[1] = 'd';
-  write(2, buf, bufl);
-  return NULL;
+	struct thread_data *dat = (struct thread_data *)cookie;
+	pami_result_t rc;
+	int lrc;
+	int last = 0;
+	char buf[64];
+	sprintf(buf, "Ra %ld\n", pthread_self());
+	int bufl = strlen(buf);
+	write(2, buf, bufl);
+	while (run) {
+		while (run && dat->active) {
+			if (last == 0) {
+				buf[0] = 'T'; buf[1] = 'o';
+				write(2, buf, bufl);
+				last = 1;
+			}
+			rc = PAMI_Context_trylock(dat->context); // should cause wakeups...
+			if (rc == PAMI_SUCCESS) {
+				while (run && dat->active) {
+					// why doesn't advance return "num events" anymore?
+					rc = PAMI_Context_advance(dat->context, 100);
+				}
+				rc = PAMI_Context_unlock(dat->context);
+				// might as well just 'break' here...
+			}
+		}
+		last = 0;
+		buf[0] = 'G'; buf[1] = 'b';
+		write(2, buf, bufl);
+		lrc = pthread_mutex_lock(&dat->mutex);
+		// now, active == 1 || run == 0
+		pthread_mutex_unlock(&dat->mutex);
+	}
+	buf[0] = 'A'; buf[1] = 'd';
+	write(2, buf, bufl);
+	return NULL;
 }
 
 struct thread_data thr_data[NUM_CONTEXTS];
 
 int main(int argc, char ** argv) {
-  pami_client_t client;
-  pami_configuration_t *configuration = NULL;
-  char cl_string[] = "TEST";
-  pami_result_t result = PAMI_ERROR;
-  pami_context_t context[NUM_CONTEXTS];
-  int x, y;
-  char buf[64];
-  sprintf(buf, "St %ld\n", pthread_self());
-  int bufl = strlen(buf);
+	pami_client_t client;
+	char cl_string[] = "TEST";
+	pami_result_t result = PAMI_ERROR;
+	pami_context_t context[NUM_CONTEXTS];
+	int x, y;
+	char buf[64];
+	sprintf(buf, "St %ld\n", pthread_self());
+	int bufl = strlen(buf);
 
-  result = PAMI_Client_create(cl_string, &client);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to initialize pami client. "
-            "result = %d\n", result);
-    return 1;
-  }
+	result = PAMI_Client_create(cl_string, &client);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to initialize pami client. "
+						"result = %d\n", result);
+		return 1;
+	}
+	pami_configuration_t configuration;
+	configuration.name = PAMI_NUM_TASKS;
+	result = PAMI_Configuration_query(client, &configuration);
+	pami_task_t ntasks = configuration.value.intval;
+	if (ntasks != 1) {
+		fprintf(stderr, "Must be run with one task\n");
+		exit(1);
+	}
 
-  result = PAMI_Context_createv(client, configuration, 0, &context[0], NUM_CONTEXTS);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to create %d pami context. "
-            "result = %d\n", NUM_CONTEXTS, result);
-    return 1;
-  }
-  write(2, buf, bufl);
-  for (x = 0; x < NUM_CONTEXTS; ++x) {
-    result = PAMI_Client_add_commthread_context(client, context[x]);
-    if (result != PAMI_SUCCESS) {
-      fprintf(stderr, "Error. Unable to add commthread to context[%d]. "
-              "result = %d (%d)\n", x, result, errno);
-      return 1;
-    }
-  }
-  for (y = 0; y < NUM_TESTRUNS; ++y) {
-    for (x = 0; x < NUM_CONTEXTS; ++x) {
-      _info[x].seq = y * NUM_CONTEXTS + x + 1;
-    }
+	result = PAMI_Context_createv(client, NULL, 0, &context[0], NUM_CONTEXTS);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to create %d pami context. "
+						"result = %d\n", NUM_CONTEXTS, result);
+		return 1;
+	}
+	write(2, buf, bufl);
+	for (x = 0; x < NUM_CONTEXTS; ++x) {
+		result = PAMI_Client_add_commthread_context(client, context[x]);
+		if (result != PAMI_SUCCESS) {
+			fprintf(stderr, "Error. Unable to add commthread to context[%d]. "
+							"result = %d (%d)\n", x, result, errno);
+			return 1;
+		}
+	}
+	for (y = 0; y < NUM_TESTRUNS; ++y) {
+		for (x = 0; x < NUM_CONTEXTS; ++x) {
+			_info[x].seq = y * NUM_CONTEXTS + x + 1;
+		}
 
-    if (y & 1) {
-      if (run) {
-        if (y & 2) {
-          for (x = 0; x < NUM_CONTEXTS; ++x) {
-            thr_data[x].active = 1;
-            pthread_mutex_unlock(&thr_data[x].mutex);
-          }
-        } else {
-          for (x = 0; x < NUM_CONTEXTS; ++x) {
-            // we shouldn't wait at all...
-            pthread_mutex_lock(&thr_data[x].mutex);
-            thr_data[x].active = 0;
-          }
-        }
-      } else {
-        run = 1;
-        for (x = 0; x < NUM_CONTEXTS; ++x) {
-          pthread_attr_t attr;
-          thr_data[x].context = context[x];
-          thr_data[x].active = 0;
-          pthread_mutex_init(&thr_data[x].mutex, NULL);
-          pthread_mutex_lock(&thr_data[x].mutex);
-          pthread_attr_init(&attr);
+		if (y & 1) {
+			if (run) {
+				if (y & 2) {
+					for (x = 0; x < NUM_CONTEXTS; ++x) {
+						thr_data[x].active = 1;
+						pthread_mutex_unlock(&thr_data[x].mutex);
+					}
+				} else {
+					for (x = 0; x < NUM_CONTEXTS; ++x) {
+						// we shouldn't wait at all...
+						pthread_mutex_lock(&thr_data[x].mutex);
+						thr_data[x].active = 0;
+					}
+				}
+			} else {
+				run = 1;
+				for (x = 0; x < NUM_CONTEXTS; ++x) {
+					pthread_attr_t attr;
+					thr_data[x].context = context[x];
+					thr_data[x].active = 0;
+					pthread_mutex_init(&thr_data[x].mutex, NULL);
+					pthread_mutex_lock(&thr_data[x].mutex);
+					pthread_attr_init(&attr);
 #ifdef __pami_target_bgq__
-          cpu_set_t cpu_mask;
-          CPU_ZERO(&cpu_mask);
-          CPU_SET((x + 1) * 4, &cpu_mask);
-          pthread_attr_setaffinity_np(&attr,
-                                      sizeof(cpu_set_t),
-                                      &cpu_mask);
+					cpu_set_t cpu_mask;
+					CPU_ZERO(&cpu_mask);
+					CPU_SET((x + 1) * 4, &cpu_mask);
+					pthread_attr_setaffinity_np(&attr,
+																			sizeof(cpu_set_t),
+																			&cpu_mask);
 #endif // __pami_target_bgq__
-          int rc = pthread_create(&thr_data[x].thread, &attr,
-                                  user_pthread, (void *)&thr_data[x]);
-          if (rc == -1) perror("pthread_create");
-          pthread_attr_destroy(&attr);
-        }
-      }
-    }
+					int rc = pthread_create(&thr_data[x].thread, &attr,
+																	user_pthread, (void *)&thr_data[x]);
+					if (rc == -1) perror("pthread_create");
+					pthread_attr_destroy(&attr);
+				}
+			}
+		}
 
-    result = run_test(client, context, NUM_CONTEXTS);
-    if (result != PAMI_SUCCESS) {
-      fprintf(stderr, "Error. Unable to run commthread test. "
-              "result = %d\n", result);
-      //return 1;
-      break;
-    }
+		result = run_test(client, context, NUM_CONTEXTS);
+		if (result != PAMI_SUCCESS) {
+			fprintf(stderr, "Error. Unable to run commthread test. "
+							"result = %d\n", result);
+			//return 1;
+			break;
+		}
 
-    if (y + 1 < NUM_TESTRUNS) {
-      buf[0] = 'S'; buf[1] = 'p';
-      write(2, buf, bufl);
-      fprintf(stderr, "Sleeping...\n");
-      //			sleep(5);
-      unsigned long long t0 = PAMI_Wtimebase();
-      //fprintf(stderr, "Woke up after %lld cycles\n", (PAMI_Wtimebase() - t0));
-      while (PAMI_Wtimebase() - t0 < 500000);
-      buf[0] = 'W'; buf[1] = 'a';
-      write(2, buf, bufl);
-    }
-  }
-  run = 0;
+		if (y + 1 < NUM_TESTRUNS) {
+			buf[0] = 'S'; buf[1] = 'p';
+			write(2, buf, bufl);
+			fprintf(stderr, "Sleeping...\n");
+			//			sleep(5);
+			unsigned long long t0 = PAMI_Wtimebase();
+			//fprintf(stderr, "Woke up after %lld cycles\n", (PAMI_Wtimebase() - t0));
+			while (PAMI_Wtimebase() - t0 < 500000);
+			buf[0] = 'W'; buf[1] = 'a';
+			write(2, buf, bufl);
+		}
+	}
+	run = 0;
 
-  buf[0] = 'S'; buf[1] = 'p';
-  write(2, buf, bufl);
-  {unsigned long long t0 = PAMI_Wtimebase();
-    while (PAMI_Wtimebase() - t0 < 500000);}
-  buf[0] = 'F'; buf[1] = 'i';
-  write(2, buf, bufl);
-  for (x = 0; x < NUM_CONTEXTS; ++x) {
-    pthread_mutex_unlock(&thr_data[x].mutex);
-  }
-  result = PAMI_Context_destroyv(context, NUM_CONTEXTS);
-  for (x = 0; x < NUM_CONTEXTS; ++x) {
-    pthread_join(thr_data[x].thread, NULL);
-  }
-  result = PAMI_Client_destroy(&client);
-  if (result != PAMI_SUCCESS) {
-    fprintf(stderr, "Error. Unable to finalize pami client. "
-            "result = %d\n", result);
-    return 1;
-  }
+	buf[0] = 'S'; buf[1] = 'p';
+	write(2, buf, bufl);
+	{unsigned long long t0 = PAMI_Wtimebase();
+		while (PAMI_Wtimebase() - t0 < 500000);}
+	buf[0] = 'F'; buf[1] = 'i';
+	write(2, buf, bufl);
+	for (x = 0; x < NUM_CONTEXTS; ++x) {
+		pthread_mutex_unlock(&thr_data[x].mutex);
+	}
+	result = PAMI_Context_destroyv(context, NUM_CONTEXTS);
+	for (x = 0; x < NUM_CONTEXTS; ++x) {
+		pthread_join(thr_data[x].thread, NULL);
+	}
+	result = PAMI_Client_destroy(&client);
+	if (result != PAMI_SUCCESS) {
+		fprintf(stderr, "Error. Unable to finalize pami client. "
+						"result = %d\n", result);
+		return 1;
+	}
 
-  buf[0] = 'S'; buf[1] = 'u';
-  write(2, buf, bufl);
-  fprintf(stderr, "Success.\n");
+	buf[0] = 'S'; buf[1] = 'u';
+	write(2, buf, bufl);
+	fprintf(stderr, "Success.\n");
 
-  return 0;
+	return 0;
 }
