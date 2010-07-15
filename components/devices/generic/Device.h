5ce67f278cd1998e05d8566bac5599463822f2a9
Douglas Miller <dougmill@us.ibm.com>
2010-07-15 07:30:07 -0500

    fix compile warning when not compiling BGQ

--- components/devices/generic/Device.h
+++ components/devices/generic/Device.h
@@ -292,7 +292,8 @@ public:
 		size_t myix = __global.topology_local.rank2Index(__global.mapping.task());
 		__global._wuRegion_mms[client][myix].memalign((void **)&__queues, sizeof(void *), sizeof(*__queues));
 #else // ! __pami_target_bgq__
-//		int rc = posix_memalign((void **)&__queues, sizeof(void *), sizeof(*__queues));
+		int rc = posix_memalign((void **)&__queues, sizeof(void *), sizeof(*__queues));
+		rc = rc; // until we decide what to do with error
 #endif // ! __pami_target_bgq__
 		PAMI_assertf(__queues, "Out of memory allocating generic device queues");
                 __queues->__Threads.init(mm);
@@ -300,7 +301,6 @@ public:
 #ifndef QUEUE_NO_ITER
 		__queues->__Threads.iter_init(&__ThrIter);
 #endif // !QUEUE_NO_ITER
-// should we return rc or something?
                 return PAMI_SUCCESS;
         }
 
