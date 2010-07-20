cbd484b185d6567be9e2f722d9f5d20047f9f738
Bin Jia <binj@us.ibm.com>
2010-07-20 09:25:19 -0400

    fix build break, use inline assembly to replace Xlc built-in in Arch.h(ppc64/power7)

--- arch/ppc64/Arch.h
+++ arch/ppc64/Arch.h
@@ -18,11 +18,9 @@
 
 // Here go things specific to this processor architecture
 
-//#define mem_sync()    do { asm volatile ("sync" : : : "memory"); } while(0)
+#define mem_sync()    do { asm volatile ("sync" : : : "memory"); } while(0)
 //#define mem_isync()   mem_sync()
-//#define mem_barrier() do { asm volatile ("eieio"  : : : "memory"); } while(0)
+#define mem_isync()   do { asm volatile ("isync" : : : "memory"); } while(0)
+#define mem_barrier() do { asm volatile ("lwsync"  : : : "memory"); } while(0)
 
-#define mem_sync()    __sync()
-#define mem_isync()   __isync()
-#define mem_barrier() __lwsync()
 #endif // __pami_arch_ppc64_h__
