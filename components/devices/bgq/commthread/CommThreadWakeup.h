ba59888153a6a5383f931ea890d0f03bf84009e4
Douglas Miller <dougmill@us.ibm.com>
2010-11-09 11:15:17 -0600

    temp fix while SPIs are getting updated everywhere

--- components/devices/bgq/commthread/CommThreadWakeup.h
+++ components/devices/bgq/commthread/CommThreadWakeup.h
@@ -36,7 +36,9 @@
 #include "hwi/include/bqc/wu_mmio.h"
 #define USR_WAKEUP_BASE ((unsigned long *)(PHYMAP_MINADDR_L1P + 0x1c00))
 
+#ifndef CNK_WAKEUP_SPI_FIRST_WAC
 #define CNK_WAKEUP_SPI_FIRST_WAC	0	// TBD: get from CNK
+#endif // CNK_WAKEUP_SPI_FIRST_WAC
 #define WU_ArmWithAddress(addr,mask) {				\
 	int thr = Kernel_PhysicalHWThreadID();			\
 	int reg = thr + CNK_WAKEUP_SPI_FIRST_WAC;		\
