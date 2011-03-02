b132f82cbb039c29288b47a83bc922002498c6dd
Michael Blocksome <blocksom@us.ibm.com>
2011-03-02 15:49:40 -0600

    Trac #338: astyle send immediate latency test

--- test/api/p2p/sendimmediate_latency.c
+++ test/api/p2p/sendimmediate_latency.c
@@ -58,23 +58,23 @@ static void decrement (pami_context_t   context,
                        pami_result_t    result)
 {
   unsigned * value = (unsigned *) cookie;
-  TRACE_ERR((stderr, "(%zu) decrement() cookie = %p, %d => %d\n", _my_rank, cookie, *value, *value-1));
+  TRACE_ERR((stderr, "(%zu) decrement() cookie = %p, %d => %d\n", _my_rank, cookie, *value, *value - 1));
   --*value;
 }
 #endif
 /* --------------------------------------------------------------- */
 static void test_dispatch (
-    pami_context_t        context,      /**< IN: PAMI context */
-    void               * cookie,       /**< IN: dispatch cookie */
-    const void         * header_addr,  /**< IN: header address */
-    size_t               header_size,  /**< IN: header size */
-    const void         * pipe_addr,    /**< IN: address of PAMI pipe buffer */
-    size_t               pipe_size,    /**< IN: size of PAMI pipe buffer */
-    pami_endpoint_t origin,
-pami_recv_t         * recv)        /**< OUT: receive message structure */
+  pami_context_t        context,      /**< IN: PAMI context */
+  void               * cookie,       /**< IN: dispatch cookie */
+  const void         * header_addr,  /**< IN: header address */
+  size_t               header_size,  /**< IN: header size */
+  const void         * pipe_addr,    /**< IN: address of PAMI pipe buffer */
+  size_t               pipe_size,    /**< IN: size of PAMI pipe buffer */
+  pami_endpoint_t origin,
+  pami_recv_t         * recv)        /**< OUT: receive message structure */
 {
   unsigned * value = (unsigned *) cookie;
-  TRACE_ERR((stderr, "(%zu) short recv:  decrement cookie = %p, %d => %d\n", _my_rank, cookie, *value, *value-1));
+  TRACE_ERR((stderr, "(%zu) short recv:  decrement cookie = %p, %d => %d\n", _my_rank, cookie, *value, *value - 1));
   --*value;
   _recv_iteration++;
 }
@@ -89,6 +89,7 @@ void send_once (pami_context_t context, pami_send_immediate_t * parameters)
 void recv_once (pami_context_t context)
 {
   TRACE_ERR((stderr, "(%zu) recv_once() Before advance\n", _my_rank));
+
   while (_recv_active) PAMI_Context_advance (context, 100);
 
   _recv_active = 1;
@@ -117,33 +118,37 @@ unsigned long long test (pami_context_t context, size_t dispatch, size_t hdrlen,
 
   unsigned i;
   unsigned long long t1 = PAMI_Wtimebase();
+
   if (myrank == 0)
-  {
-    parameters.dest = target;
-    for (i = 0; i < ITERATIONS; i++)
     {
-      TRACE_ERR((stderr, "(%u) Starting Iteration %d of size %zu\n", myrank, i, sndlen));
-      send_once (context, &parameters);
-      _send_active = 1;
-      recv_once (context);
-      _recv_active = 1;
+      parameters.dest = target;
+
+      for (i = 0; i < ITERATIONS; i++)
+        {
+          TRACE_ERR((stderr, "(%u) Starting Iteration %d of size %zu\n", myrank, i, sndlen));
+          send_once (context, &parameters);
+          _send_active = 1;
+          recv_once (context);
+          _recv_active = 1;
+        }
     }
-  }
   else if (myrank == 1)
-  {
-    parameters.dest = origin;
-    for (i = 0; i < ITERATIONS; i++)
     {
-      TRACE_ERR((stderr, "(%u) Starting Iteration %d of size %zu\n", myrank, i, sndlen));
-      recv_once (context);
-      _recv_active = 1;
-      send_once (context, &parameters);
-      _send_active = 1;
+      parameters.dest = origin;
+
+      for (i = 0; i < ITERATIONS; i++)
+        {
+          TRACE_ERR((stderr, "(%u) Starting Iteration %d of size %zu\n", myrank, i, sndlen));
+          recv_once (context);
+          _recv_active = 1;
+          send_once (context, &parameters);
+          _send_active = 1;
+        }
     }
-  }
+
   unsigned long long t2 = PAMI_Wtimebase();
 
-  return ((t2-t1)/ITERATIONS)/2;
+  return ((t2 - t1) / ITERATIONS) / 2;
 }
 
 int main (int argc, char ** argv)
@@ -154,13 +159,14 @@ int main (int argc, char ** argv)
   hdrsize[0] = 0;
 
   int arg;
-  for (arg=1; arg<argc; arg++)
-  {
-    hdrsize[arg] = (size_t) strtol (argv[arg], NULL, 10);
-  }
+
+  for (arg = 1; arg < argc; arg++)
+    {
+      hdrsize[arg] = (size_t) strtol (argv[arg], NULL, 10);
+    }
 
   pami_client_t client;
-  char clientname[]="PAMI";
+  char clientname[] = "PAMI";
   TRACE_ERR((stderr, "... before PAMI_Client_create()\n"));
   PAMI_Client_create (clientname, &client, NULL, 0);
   TRACE_ERR((stderr, "...  after PAMI_Client_create()\n"));
@@ -177,107 +183,118 @@ int main (int argc, char ** argv)
 
   pami_dispatch_callback_function fn;
   fn.p2p = test_dispatch;
-  pami_dispatch_hint_t options={};
+  pami_dispatch_hint_t options = {};
   TRACE_ERR((stderr, "Before PAMI_Dispatch_set() .. &_recv_active = %p, recv_active = %u\n", &_recv_active, _recv_active));
   pami_result_t result = PAMI_Dispatch_set (context,
-                                          _dispatch[_dispatch_count++],
-                                          fn,
-                                          (void *)&_recv_active,
-                                          options);
+                                            _dispatch[_dispatch_count++],
+                                            fn,
+                                            (void *) & _recv_active,
+                                            options);
+
   if (result != PAMI_SUCCESS)
-  {
-    fprintf (stderr, "Error. Unable register pami dispatch. result = %d\n", result);
-    return 1;
-  }
+    {
+      fprintf (stderr, "Error. Unable register pami dispatch. result = %d\n", result);
+      return 1;
+    }
 
   pami_configuration_t configuration;
 
   configuration.name = PAMI_CLIENT_TASK_ID;
-  result = PAMI_Client_query(client, &configuration,1);
+  result = PAMI_Client_query(client, &configuration, 1);
   _my_rank = configuration.value.intval;
 
   configuration.name = PAMI_CLIENT_NUM_TASKS;
-  result = PAMI_Client_query(client, &configuration,1);
+  result = PAMI_Client_query(client, &configuration, 1);
 
   configuration.name = PAMI_CLIENT_WTICK;
-  result = PAMI_Client_query(client, &configuration,1);
+  result = PAMI_Client_query(client, &configuration, 1);
   double tick = configuration.value.doubleval;
 
   configuration.name = PAMI_DISPATCH_SEND_IMMEDIATE_MAX;
-  result = PAMI_Dispatch_query(context, _dispatch[0], &configuration,1);
+  result = PAMI_Dispatch_query(context, _dispatch[0], &configuration, 1);
   size_t send_immediate_max = 256;
+
   if (result == PAMI_SUCCESS)
-  {
-    send_immediate_max = configuration.value.intval;
-  }
+    {
+      send_immediate_max = configuration.value.intval;
+    }
   else
-  {
-    fprintf (stderr, "Warning. Unable to query PAMI_DISPATCH_SEND_IMMEDIATE_MAX, using test default value %zu. result = %d\n", send_immediate_max, result);
-  }
+    {
+      fprintf (stderr, "Warning. Unable to query PAMI_DISPATCH_SEND_IMMEDIATE_MAX, using test default value %zu. result = %d\n", send_immediate_max, result);
+    }
 
   /* Display some test header information */
   if (_my_rank == 0)
-  {
-    char str[2][1024];
-    int index[2];
-    index[0] = 0;
-    index[1] = 0;
-
-    index[0] += sprintf (&str[0][index[0]], "#          ");
-    index[1] += sprintf (&str[1][index[1]], "#    bytes ");
-
-    fprintf (stdout, "# PAMI_Send_immediate() nearest-neighor half-pingpong blocking latency performance test\n");
-    fprintf (stdout, "#\n");
-
-    unsigned i;
-    for (i=0; i<hdrcnt; i++)
     {
-      if (i==0)
-        fprintf (stdout, "# testcase %d : header bytes = %3zd\n", i, hdrsize[i]);
-      else
-        fprintf (stdout, "# testcase %d : header bytes = %3zd (argv[%d])\n", i, hdrsize[i], i);
-      index[0] += sprintf (&str[0][index[0]], "[- testcase %d -] ", i);
-      index[1] += sprintf (&str[1][index[1]], " cycles    usec  ");
+      char str[2][1024];
+      int index[2];
+      index[0] = 0;
+      index[1] = 0;
+
+      index[0] += sprintf (&str[0][index[0]], "#          ");
+      index[1] += sprintf (&str[1][index[1]], "#    bytes ");
+
+      fprintf (stdout, "# PAMI_Send_immediate() nearest-neighor half-pingpong blocking latency performance test\n");
+      fprintf (stdout, "#\n");
+
+      unsigned i;
+
+      for (i = 0; i < hdrcnt; i++)
+        {
+          if (i == 0)
+            fprintf (stdout, "# testcase %d : header bytes = %3zd\n", i, hdrsize[i]);
+          else
+            fprintf (stdout, "# testcase %d : header bytes = %3zd (argv[%d])\n", i, hdrsize[i], i);
+
+          index[0] += sprintf (&str[0][index[0]], "[- testcase %d -] ", i);
+          index[1] += sprintf (&str[1][index[1]], " cycles    usec  ");
+        }
+
+      fprintf (stdout, "#\n");
+      fprintf (stdout, "%s\n", str[0]);
+      fprintf (stdout, "%s\n", str[1]);
+      fflush (stdout);
     }
 
-    fprintf (stdout, "#\n");
-    fprintf (stdout, "%s\n", str[0]);
-    fprintf (stdout, "%s\n", str[1]);
-    fflush (stdout);
-  }
-
   unsigned i = 0;
-  for (i = 0; i < 10000000;i++){};
+
+  for (i = 0; i < 10000000; i++) {};
 
   unsigned long long cycles;
+
   double usec;
 
   pami_endpoint_t origin, target;
+
   PAMI_Endpoint_create (client, 0, 0, &origin);
+
   PAMI_Endpoint_create (client, 1, 0, &target);
 
   char str[10240];
+
   size_t sndlen;
-  for (sndlen = 0; sndlen < send_immediate_max; sndlen = sndlen*3/2+1)
-  {
-    int index = 0;
-    index += sprintf (&str[index], "%10zd ", sndlen);
 
-    unsigned i = 0;
-    for (i=0; i<hdrcnt; i++)
+  for (sndlen = 0; sndlen < send_immediate_max; sndlen = sndlen * 3 / 2 + 1)
     {
-#ifdef WARMUP
-      test (context, _dispatch[0], hdrsize[i], sndlen, _my_rank, origin, target);
-#endif
-      cycles = test (context, _dispatch[0], hdrsize[i], sndlen, _my_rank, origin, target);
-      usec   = cycles * tick * 1000000.0;
-      /*index += sprintf (&str[index], "%7lld %7.4f  ", cycles, usec);*/
-      index += sprintf (&str[index], "%7lld  ", cycles);
-    }
+      int index = 0;
+      index += sprintf (&str[index], "%10zd ", sndlen);
 
-    if (_my_rank == 0)
-      fprintf (stdout, "%s\n", str);
-  }
+      unsigned i = 0;
+
+      for (i = 0; i < hdrcnt; i++)
+        {
+#ifdef WARMUP
+          test (context, _dispatch[0], hdrsize[i], sndlen, _my_rank, origin, target);
+#endif
+          cycles = test (context, _dispatch[0], hdrsize[i], sndlen, _my_rank, origin, target);
+          usec   = cycles * tick * 1000000.0;
+          /*index += sprintf (&str[index], "%7lld %7.4f  ", cycles, usec);*/
+          index += sprintf (&str[index], "%7lld  ", cycles);
+        }
+
+      if (_my_rank == 0)
+        fprintf (stdout, "%s\n", str);
+    }
 
   PAMI_Client_destroy(&client);
 
