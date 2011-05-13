6007ffef484a4fb3ea5f1bf0dd2ce02fe93e9d17
Bob Cernohous <bobc@us.ibm.com>
2011-05-13 16:41:52 -0500

    Update scan.c testcase and fix GNUMakefile.in (again)

--- test/api/collectives/scan.c
+++ test/api/collectives/scan.c
@@ -8,17 +8,15 @@
 /* end_generated_IBM_copyright_prolog                               */
 /**
  * \file test/api/collectives/scan.c
- * \brief ???
+ * \brief Simple scan on world geometry
  */
 
-/*define this if you want to validate the data for unsigned sums */
-
+/* see setup_env() for environment variable overrides
+#define OFFSET     0
+ */
 #define FULL_TEST  1
 #define COUNT      65536
-#define MAXBUFSIZE COUNT*16
-#define NITERLAT   100
-#define NITERBW    10
-#define CUTOFF     65536
+#define NITERLAT   10
 
 #include "../pami_util.h"
 
@@ -55,8 +53,7 @@ int check_rcvbuf (void *buf, int count, int op, int dt, int num_tasks, int task_
     {
       if (rbuf[i] != i * (task_id+1))
       {
-        fprintf(stderr,"Check(%d) failed rbuf[%d] %u != %u\n",count,i,rbuf[i],i*(num_tasks-task_id));
-        while (1);
+        fprintf(stderr,"Check(%d) failed rbuf[%d] %u != %u\n",count,i,rbuf[i],i*(task_id+1));
         err = -1;
         return err;    
       }
@@ -70,8 +67,7 @@ int check_rcvbuf (void *buf, int count, int op, int dt, int num_tasks, int task_
 int main(int argc, char*argv[])
 {
   pami_client_t        client;
-  pami_context_t       context;
-  size_t               num_contexts=1;
+  pami_context_t      *context;
   pami_task_t          task_id,task_zero=0;
   size_t               num_tasks;
   pami_geometry_t      world_geometry;
@@ -99,212 +95,173 @@ int main(int argc, char*argv[])
   pami_xfer_t          barrier;
   pami_xfer_t          scan;
 
+  /* Process environment variables and setup globals */
+  setup_env();
 
-  char sbuf[MAXBUFSIZE];
-  char rbuf[MAXBUFSIZE];
-  int op, dt;
+  assert(gNum_contexts > 0);
+  context = (pami_context_t*)malloc(sizeof(pami_context_t) * gNum_contexts);
 
-  /* \note Test environment variable" TEST_VERBOSE=N     */
-  char* sVerbose = getenv("TEST_VERBOSE");
-
-  if (sVerbose) gVerbose=atoi(sVerbose); /* set the global defined in coll_util.h */
-
-  /* \note Test environment variable" TEST_PROTOCOL={-}substring.       */
-  /* substring is used to select, or de-select (with -) test protocols */
-  unsigned selector = 1;
-  char* selected = getenv("TEST_PROTOCOL");
-  if (!selected) selected = "";
-  else if (selected[0]=='-')
-  {
-    selector = 0 ;
-    ++selected;
-  }
-
+  /*  Allocate buffer(s) */
+  int err = 0;
+  void* sbuf = NULL;
+  err = posix_memalign(&sbuf, 128, MAXBUFSIZE + gBuffer_offset);
+  assert(err == 0);
+  sbuf = (char*)sbuf + gBuffer_offset;
+  void* rbuf = NULL;
+  err = posix_memalign(&rbuf, 128, MAXBUFSIZE + gBuffer_offset);
+  assert(err == 0);
+  rbuf = (char*)rbuf + gBuffer_offset;
 
   /*  Initialize PAMI */
   int rc = pami_init(&client,        /* Client             */
-                     &context,       /* Context            */
+                     context,        /* Context            */
                      NULL,           /* Clientname=default */
-                     &num_contexts,  /* num_contexts       */
+                     &gNum_contexts, /* gNum_contexts       */
                      NULL,           /* null configuration */
                      0,              /* no configuration   */
                      &task_id,       /* task id            */
                      &num_tasks);    /* number of tasks    */
+
   if (rc==1)
     return 1;
 
   /*  Query the world geometry for barrier algorithms */
   rc |= query_geometry_world(client,
-                            context,
-                            &world_geometry,
-                            barrier_xfer,
-                            barrier_num_algorithm,
-                            &bar_always_works_algo,
-                            &bar_always_works_md,
-                            &bar_must_query_algo,
-                            &bar_must_query_md);
+                             context[0],
+                             &world_geometry,
+                             barrier_xfer,
+                             barrier_num_algorithm,
+                             &bar_always_works_algo,
+                             &bar_always_works_md,
+                             &bar_must_query_algo,
+                             &bar_must_query_md);
+
   if (rc==1)
     return 1;
 
-  /*  Query the world geometry for scan algorithms */
-  rc |= query_geometry_world(client,
-                            context,
-                            &world_geometry,
-                            scan_xfer,
-                            scan_num_algorithm,
-                            &scan_always_works_algo,
-                            &scan_always_works_md,
-                            &scan_must_query_algo,
-                            &scan_must_query_md);
-  if (rc==1)
-    return 1;
+  barrier.cb_done   = cb_done;
+  barrier.cookie    = (void*) & bar_poll_flag;
+  barrier.algorithm = bar_always_works_algo[0];
 
-  size_t** validTable=
-  alloc2DContig(op_count,dt_count);
-#if FULL_TEST
-  for (i=0;i<op_count;i++)
-    for (j=0;j<dt_count;j++)
-      validTable[i][j]=1;
+  unsigned iContext = 0;
 
-  /*--------------------------------------*/
-  /* Disable unsupported ops on complex   */
-  /* Only sum, prod                       */
-  for (i = 0, j = DT_SINGLE_COMPLEX; i < OP_COUNT; i++)if (i!=OP_SUM && i!=OP_PROD) validTable[i][j] = 0;
-  for (i = 0, j = DT_DOUBLE_COMPLEX; i < OP_COUNT; i++)if (i!=OP_SUM && i!=OP_PROD) validTable[i][j] = 0;
-
-    /*--------------------------------------*/
-    /* Disable non-LOC ops on LOC dt's      */
-  for (i = 0, j = DT_LOC_2INT      ; i < OP_MAXLOC; i++)validTable[i][j] = 0;
-  for (i = 0, j = DT_LOC_SHORT_INT ; i < OP_MAXLOC; i++)validTable[i][j] = 0;
-  for (i = 0, j = DT_LOC_FLOAT_INT ; i < OP_MAXLOC; i++)validTable[i][j] = 0;
-  for (i = 0, j = DT_LOC_DOUBLE_INT; i < OP_MAXLOC; i++)validTable[i][j] = 0;
-  for (i = 0, j = DT_LOC_2FLOAT    ; i < OP_MAXLOC; i++)validTable[i][j] = 0;
-  for (i = 0, j = DT_LOC_2DOUBLE   ; i < OP_MAXLOC; i++)validTable[i][j] = 0;
-
-  /*--------------------------------------*/
-  /* Disable NULL and byte operations     */
-  for (i = 0, j = DT_NULL; i < OP_COUNT; i++)validTable[i][j] = 0;
-  for (i = 0, j = DT_BYTE; i < OP_COUNT; i++)validTable[i][j] = 0;
-  for (j = 0, i = OP_COPY; j < DT_COUNT; j++) validTable[i][j] = 0;
-  for (j = 0, i = OP_NOOP; j < DT_COUNT; j++) validTable[i][j] = 0;
-
-  /*--------------------------------------*/
-  /* Disable LOC ops on non-LOC dt's      */
-  for (j = 0, i = OP_MAXLOC; j < DT_LOC_2INT; j++) validTable[i][j] = 0;
-  for (j = 0, i = OP_MINLOC; j < DT_LOC_2INT; j++) validTable[i][j] = 0;
-
-  /*---------------------------------------*/
-  /* Disable unsupported ops on logical dt */
-  /* Only land, lor, lxor, band, bor, bxor */
-  for (i = 0,         j = DT_LOGICAL; i < OP_LAND ; i++) validTable[i][j] = 0;
-  for (i = OP_BXOR+1, j = DT_LOGICAL; i < OP_COUNT; i++) validTable[i][j] = 0;
-
-  /*---------------------------------------*/
-  /* Disable unsupported ops on long double*/
-  /* Only max,min,sum,prod                 */
-  for (i = OP_PROD+1, j = DT_LONG_DOUBLE; i < OP_COUNT; i++) validTable[i][j] = 0;
-
-#else
-  for (i=0;i<op_count;i++)
-    for (j=0;j<dt_count;j++)
-      validTable[i][j]=0;
-
-  validTable[OP_SUM][DT_UNSIGNED_INT]=1;
-
-#endif
-
-  for (nalg=0; nalg<scan_num_algorithm[0]; nalg++)
+  for (; iContext < gNum_contexts; ++iContext)
   {
-    if (task_id == task_zero)
+
+    if (task_id == 0)
+      printf("# Context: %u\n", iContext);
+
+    /*  Query the world geometry for scan algorithms */
+    rc |= query_geometry_world(client,
+                               context[iContext],
+                               &world_geometry,
+                               scan_xfer,
+                               scan_num_algorithm,
+                               &scan_always_works_algo,
+                               &scan_always_works_md,
+                               &scan_must_query_algo,
+                               &scan_must_query_md);
+    if (rc==1)
+      return 1;
+
+    for (nalg = 0; nalg < scan_num_algorithm[0]; nalg++)
     {
-      printf("# Scan Bandwidth Test -- task_zero = %d protocol: %s\n", task_zero,
-             scan_always_works_md[nalg].name);
-      printf("# Size(bytes)           cycles    bytes/sec    usec\n");
-      printf("# -----------      -----------    -----------    ---------\n");
-    }
-    if (((strstr(scan_always_works_md[nalg].name,selected) == NULL) && selector) ||
-        ((strstr(scan_always_works_md[nalg].name,selected) != NULL) && !selector))  continue;
-
-    barrier.cb_done   = cb_done;
-    barrier.cookie    = (void*)&bar_poll_flag;
-    barrier.algorithm = bar_always_works_algo[0];
-    blocking_coll(context,&barrier,&bar_poll_flag);
-
-    scan.cb_done   = cb_done;
-    scan.cookie    = (void*)&scan_poll_flag;
-    scan.algorithm = scan_always_works_algo[nalg];
-    scan.cmd.xfer_scan.sndbuf    = sbuf;
-    scan.cmd.xfer_scan.stype     = PAMI_TYPE_BYTE;
-    scan.cmd.xfer_scan.stypecount= 0;
-    scan.cmd.xfer_scan.rcvbuf    = rbuf;
-    scan.cmd.xfer_scan.rtype     = PAMI_TYPE_BYTE;
-    scan.cmd.xfer_scan.rtypecount= 0;
-    scan.cmd.xfer_scan.exclusive = 0;
-
-
-
-    for (dt=0; dt<dt_count; dt++)
-      for (op=0; op<op_count; op++)
+      if (task_id == task_zero) /* root not set yet */
       {
-        if (validTable[op][dt])
+        printf("# Scan Bandwidth Test -- context = %d, task_zero = %d protocol: %s\n", 
+               iContext, task_zero, scan_always_works_md[nalg].name);
+        printf("# Size(bytes)           cycles    bytes/sec    usec\n");
+        printf("# -----------      -----------    -----------    ---------\n");
+      }
+
+      if (((strstr(scan_always_works_md[nalg].name, gSelected) == NULL) && gSelector) ||
+          ((strstr(scan_always_works_md[nalg].name, gSelected) != NULL) && !gSelector))  continue;
+
+      gProtocolName = scan_always_works_md[nalg].name;
+
+      scan.cb_done   = cb_done;
+      scan.cookie    = (void*)&scan_poll_flag;
+      scan.algorithm = scan_always_works_algo[nalg];
+      scan.cmd.xfer_scan.sndbuf    = sbuf;
+      scan.cmd.xfer_scan.rcvbuf    = rbuf;
+      scan.cmd.xfer_scan.rtype     = PAMI_TYPE_BYTE;
+      scan.cmd.xfer_scan.rtypecount= 0;
+      scan.cmd.xfer_scan.exclusive = 0;
+
+      int op, dt;
+
+      for (dt=0; dt<dt_count; dt++)
+        for (op=0; op<op_count; op++)
         {
-          if (task_id == task_zero)
-            printf("Running Scan: %s, %s\n",dt_array_str[dt], op_array_str[op]);
-          for (i=4; i<=COUNT; i*=2)
+          if (gValidTable[op][dt])
           {
-            size_t sz=get_type_size(dt_array[dt]);
-            long long dataSent = i*sz;
-            int niter;
-            if (dataSent < CUTOFF)
-              niter = NITERLAT;
-            else
-              niter = NITERBW;
-
-
-            /* initialize_sndbuf (sbuf, i, op_array[op], dt_array[dt], task_id); */
-            initialize_sndbuf (sbuf, i, op, dt, task_id);
-
-            blocking_coll(context,&barrier,&bar_poll_flag);
-            ti = timer();
-            for (j=0; j<niter; j++)
+            if (task_id == task_zero)
+              printf("Running Scan: %s, %s\n",dt_array_str[dt], op_array_str[op]);
+            for (i = 1; i <= gMax_count; i *= 2)
             {
+              size_t sz=get_type_size(dt_array[dt]);
+              size_t  dataSent = i * sz;
+              int niter;
+
+              if (dataSent < CUTOFF)
+                niter = gNiterlat;
+              else
+                niter = NITERBW;
+
               scan.cmd.xfer_scan.stypecount=i;
               scan.cmd.xfer_scan.rtypecount=dataSent;
               scan.cmd.xfer_scan.stype     =dt_array[dt];
               scan.cmd.xfer_scan.op        =op_array[op];
-              blocking_coll(context, &scan, &scan_poll_flag);
-            }
-            tf = timer();
-            blocking_coll(context,&barrier,&bar_poll_flag);
 
-            int rc_check;
-            rc |= rc_check = check_rcvbuf (rbuf, i, op, dt, num_tasks, task_id); 
+              initialize_sndbuf (sbuf, i, op, dt, task_id);
+              memset(rbuf, 0xFF, dataSent);
 
-            if (rc_check) fprintf(stderr, "%s FAILED validation\n", gProtocolName);
+              /* We aren't testing barrier itself, so use context 0. */
+              blocking_coll(context[0], &barrier, &bar_poll_flag);
+              ti = timer();
+              for (j=0; j<niter; j++)
+              {
+                blocking_coll(context[iContext], &scan, &scan_poll_flag);
+              }
+              tf = timer();
+              /* We aren't testing barrier itself, so use context 0. */
+              blocking_coll(context[0], &barrier, &bar_poll_flag);
 
-            usec = (tf - ti)/(double)niter;
-            if (task_id == task_zero)
-            {
-              printf("  %11lld %16d %14.1f %12.2f\n",
-                     dataSent,
-                     niter,
-                     (double)1e6*(double)dataSent/(double)usec,
-                     usec);
-              fflush(stdout);
+              int rc_check;
+              rc |= rc_check = check_rcvbuf (rbuf, i, op, dt, num_tasks, task_id); 
+
+              if (rc_check) fprintf(stderr, "%s FAILED validation\n", gProtocolName);
+
+              usec = (tf - ti)/(double)niter;
+              if (task_id == task_zero)
+              {
+                printf("  %11lld %16d %14.1f %12.2f\n",
+                       (long long)dataSent,
+                       niter,
+                       (double)1e6*(double)dataSent/(double)usec,
+                       usec);
+                fflush(stdout);
+              }
             }
           }
         }
-      }
-  }
-  rc |= pami_shutdown(&client,&context,&num_contexts);
+    }
+    free(scan_always_works_algo);
+    free(scan_always_works_md);
+    free(scan_must_query_algo);
+    free(scan_must_query_md);
+
+  } /*for(unsigned iContext = 0; iContext < gNum_contexts; ++iContexts)*/
   free(bar_always_works_algo);
   free(bar_always_works_md);
   free(bar_must_query_algo);
   free(bar_must_query_md);
-  free(scan_always_works_algo);
-  free(scan_always_works_md);
-  free(scan_must_query_algo);
-  free(scan_must_query_md);
 
+  sbuf = (char*)sbuf - gBuffer_offset;
+  free(sbuf);
+  rbuf = (char*)rbuf - gBuffer_offset;
+  free(rbuf);
+
+  rc |= pami_shutdown(&client, context, &gNum_contexts);
   return rc;
 }
