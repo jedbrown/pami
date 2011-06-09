e61494da618eaa9443df8cfe0204837de7b5fb4f
Michael Blocksome <blocksom@us.ibm.com>
2011-06-09 11:57:54 -0500

    Trac #467: update fifo unit test to test only linear fifo class with native atomics for now.

--- test/internals/fifo/bgq/fifo_func.cc
+++ test/internals/fifo/bgq/fifo_func.cc
@@ -14,6 +14,8 @@
 
 #include "Global.h"
 
+#include "components/atomic/native/NativeCounter.h"
+#include "components/atomic/indirect/IndirectCounter.h"
 
 #include "components/devices/shmem/ShmemPacket.h"
 
@@ -23,29 +25,29 @@
 #include "components/atomic/bgq/L2CounterBounded.h"
 
 typedef PAMI::Fifo::FifoPacket <32, 160> ShmemPacket;
+
 //typedef PAMI::Fifo::WrapFifo<ShmemPacket, PAMI::BoundedCounter::BGQ::IndirectL2Bounded, 128, Wakeup::BGQ> ShmemFifo;
 typedef PAMI::Fifo::WrapFifo<ShmemPacket, PAMI::BoundedCounter::BGQ::IndirectL2Bounded, 128> Wrap;
+
 //typedef Fifo::LinearFifo<ShmemPacket, PAMI::Counter::BGQ::IndirectL2, 128, Wakeup::BGQ> ShmemFifo;
-typedef PAMI::Fifo::LinearFifo<ShmemPacket, PAMI::Counter::BGQ::IndirectL2, 128> Linear;
+//typedef PAMI::Fifo::LinearFifo<ShmemPacket, PAMI::Counter::BGQ::IndirectL2, 128> Linear;
+typedef PAMI::Fifo::LinearFifo<ShmemPacket, PAMI::Counter::Indirect<PAMI::Counter::Native>, 128> Linear;
 
-int main(int argc, char ** argv)
+
+
+int main (int argc, char ** argv)
 {
-
-
   size_t task = __global.mapping.task();
   size_t size = __global.mapping.size();
-  //fprintf (stdout, "%zu of %zu\n", task, size);
-
 
   PAMI::Memory::GenMemoryManager mm;
   char shmemfile[PAMI::Memory::MMKEYSIZE];
   snprintf (shmemfile, sizeof(shmemfile) - 1, "/foo");
   mm.init(__global.shared_mm, 8*1024*1024, 1, 1, 0, shmemfile);
 
-
-  Test<Wrap> wrap;
-  wrap.init (&mm, task, size);
-  wrap.functional("wrap fifo");
+  //Test<Wrap> wrap;
+  //wrap.init (&mm, task, size);
+  //wrap.functional("wrap fifo");
 
   Test<Linear> linear;
   linear.init (&mm, task, size);
