db94bbc00273eecb362f3058b9fd0da5ce1bab5f
Douglas Miller <dougmill@us.ibm.com>
2011-06-16 12:25:15 -0500

    Improve error messages when async_progress extension is not present

--- test/async_progress.h
+++ test/async_progress.h
@@ -17,16 +17,28 @@ static pami_result_t (*async_prog_disable)(pami_context_t context,
 static inline pami_result_t init_async_prog() {
 	pami_result_t rc;
 	rc = PAMI_Extension_open(NULL, "EXT_async_progress", &async_prog_ext);
-	if (rc != PAMI_SUCCESS) return rc;
+	if (rc != PAMI_SUCCESS) {
+		fprintf(stderr, "Required PAMI extension not present: EXT_async_progress\n");
+		return rc;
+	}
 	async_prog_register = PAMI_Extension_symbol(async_prog_ext,
 						"register");
-	if (async_prog_register == NULL) return PAMI_ERROR;
+	if (async_prog_register == NULL) {
+		fprintf(stderr, "PAMI extension EXT_async_progress has no 'register' func\n");
+		return PAMI_ERROR;
+	}
 	async_prog_enable = PAMI_Extension_symbol(async_prog_ext,
 						"enable");
-	if (async_prog_enable == NULL) return PAMI_ERROR;
+	if (async_prog_enable == NULL) {
+		fprintf(stderr, "PAMI extension EXT_async_progress has no 'enable' func\n");
+		return PAMI_ERROR;
+	}
 	async_prog_disable = PAMI_Extension_symbol(async_prog_ext,
 						"disable");
-	if (async_prog_disable == NULL) return PAMI_ERROR;
+	if (async_prog_disable == NULL) {
+		fprintf(stderr, "PAMI extension EXT_async_progress has no 'disable' func\n");
+		return PAMI_ERROR;
+	}
 	return PAMI_SUCCESS;
 }
 
