332a11e5cec0a7d8650952b3f3b309e456dd7ca8
Tsai-Yang (Alan) Jea <tjea@us.ibm.com>
2011-07-18 13:55:59 -0400

    Trac #511: Add PE specific pami_sys.h
    On PE implementation, we need 64 bytes for memory region on both 32
    and 64 bit applications.

--- sys/lapiunix/pami_sys.h
+++ sys/lapiunix/pami_sys.h
@@ -1,1 +1,19 @@
-../default/pami_sys.h
+/**
+ * \file sys/lapiunix/pami_sys.h
+ * \brief "Default" platform specifc type definitions for IBM's PAMI message layer.
+ */
+
+#ifndef __default_pami_sys_h__
+#define __default_pami_sys_h__
+
+/* We need 64 bytes memory region for both 32- and 64-bit on PE implementation */
+#ifndef __64BIT__
+#define PAMI_CLIENT_MEMREGION_SIZE_STATIC (16*sizeof(uintptr_t))
+#else
+#define PAMI_CLIENT_MEMREGION_SIZE_STATIC (8*sizeof(uintptr_t))
+#endif
+
+#define PAMI_WORK_SIZE_STATIC (8*sizeof(uintptr_t))
+#define PAMI_REQUEST_NQUADS 512
+
+#endif /* __default_pami_sys_h__ */
