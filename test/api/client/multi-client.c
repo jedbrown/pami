0fabd8e4fd72b7a818173e60c8e7d7c7e91ce3fd
Charles Archer <archerc@us.ibm.com>
2011-10-11 17:36:11 -0400

    Fix array of clients in multi-client test

--- test/api/client/multi-client.c
+++ test/api/client/multi-client.c
@@ -6,25 +6,26 @@
 #include <pami.h>
 #include <stdio.h>
 
+#define MAX_CLIENTS 2
 
 int main (int argc, char ** argv)
 {
-  pami_client_t client;
+  pami_client_t client[MAX_CLIENTS];
   pami_result_t result = PAMI_ERROR;
   char         cl_string[] = "TEST";
   
-  int max_clients=2,i;
+  int max_clients=MAX_CLIENTS,i;
   for(i=0;i<max_clients;i++)
   {
     fprintf (stdout, "Creating Client %d of %d\n", i, max_clients);
-    result = PAMI_Client_create (cl_string, &client, NULL, 0);
+    result = PAMI_Client_create (cl_string, &client[i], NULL, 0);
     if(result!=PAMI_SUCCESS)
       fprintf(stderr, "--->error creating client %d\n", i);
   }
   for(i=0;i<max_clients;i++)
   {
     fprintf (stdout, "Destroying Client %d of %d\n", i, max_clients);
-    result = PAMI_Client_destroy(&client);
+    result = PAMI_Client_destroy(&client[i]);
     if(result!=PAMI_SUCCESS)
       fprintf(stderr, "--->error destroying client %d\n", i);
   }
