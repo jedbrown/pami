jedbrown/pami

commit 8b5ef4e8d8661fd3f083f26a7ed1851a51a0ff9a

Performance improvements for Pack/Unpack operation (T260, D175012)
PAMI uses the PAMI_TYPE_CONTIGUOUS tpye code for the Pack/Unpack
operation of the user data to the dgs pool, copying one byte at a
time.

In order to improve the performance, a new type code object is used,
PAMI_TYPE_CONTIG_MAX, which has a unit size of ULONG_MAX and an
atom size of 1.  The per-byte copying is, therefore, being replaced
with copying large chunck of data, therefore drastically reducing the
number of memcpy function calls and, consequently, increasing the
performance of the Pack/Unpack operation.

In addition to adding the new type code, the default copy operation
is changed to use memcpy directly, instead of the Math::copy method.

common/type/TypeMachine.h
@@ -48,6 +48,8 @@ namespace PAMI
 
         private:
             TypeCode  *type;
+            TypeCode  *orig_type;
+            bool optimized;
             void  *cookie;
             TypeFunc::CopyFunction  copy_func;
 
@@ -90,11 +92,20 @@ namespace PAMI
 
     };
 
-    inline TypeMachine::TypeMachine(TypeCode *type)
-      : type(type),  cookie(NULL),copy_func(NULL), top(0)
+    inline TypeMachine::TypeMachine(TypeCode *atype)
+      : type(atype), optimized(false), cookie(NULL), copy_func(NULL), top(0)
     {
         assert(type->IsCompleted());
         type->AcquireReference();
+
+        // Check the type and, if contiguous, change it to
+        // a max contiguous type
+        if (type->IsContiguous()) {
+            orig_type = type;
+            optimized = true;
+            type = PAMI_TYPE_CONTIG_MAX;
+        }
+
         if (type->GetDepth() <= (unsigned)SMALL_STACK_DEPTH)
             stack = small_stack;
         else
@@ -105,6 +116,11 @@ namespace PAMI
 
     inline TypeMachine::~TypeMachine()
     {
+        // recover the original type if optimized
+        if (optimized) {
+            assert(orig_type);
+            type = orig_type;
+        }
         type->ReleaseReference();
         if (stack != small_stack)
             delete[] stack;
@@ -112,8 +128,27 @@ namespace PAMI
 
     inline void TypeMachine::SetType(TypeCode *new_type)
     {
+        assert(new_type->IsCompleted()&&(new_type!=PAMI_TYPE_CONTIG_MAX));
+
+        new_type->AcquireReference();
+
+        // recover the initial object if optimized
+        if (optimized) {
+            type = orig_type;
+            orig_type = (TypeCode *)NULL;
+            optimized = false;
+        }
+
         type->ReleaseReference();
 
+        // Check the new type and, if contiguous, change it to
+        // the max contiguous type
+        if (new_type->IsContiguous()) {
+            orig_type = new_type;
+            optimized = true;
+            new_type = PAMI_TYPE_CONTIG_MAX;
+        }
+		
         // adjust stack if new_type requires more
         size_t new_depth = new_type->GetDepth();
         if (new_depth > type->GetDepth() && new_depth > (unsigned)SMALL_STACK_DEPTH) {
@@ -122,8 +157,6 @@ namespace PAMI
             stack = new Cursor[new_depth];
         }
 
-        assert(new_type->IsCompleted());
-        new_type->AcquireReference();
         type = new_type;
 
         top  = 0;
@@ -140,6 +173,10 @@ namespace PAMI
 
     inline TypeCode *TypeMachine::GetType() const
     {
+        if (optimized) {
+            assert(orig_type);
+            return orig_type;
+        }
         return type;
     }
 

api/c/pami_type.cc
@@ -10,6 +10,8 @@ using namespace PAMI::Type;
 #define RETURN_ERR_PAMI(code, ...) return (code)
 #endif
 
+TypeContig *PAMI_TYPE_CONTIG_MAX         = new TypeContig(1);
+
 pami_type_t PAMI_TYPE_CONTIGUOUS         = new TypeContig(TypeCode::PRIMITIVE_TYPE_CONTIGUOUS);
 pami_type_t PAMI_TYPE_BYTE               = new TypeContig(TypeCode::PRIMITIVE_TYPE_BYTE);         // ----------------------???
 

common/type/TypeCode.h
@@ -382,7 +382,7 @@ namespace PAMI
         assert(!IsCompleted());
 
         if (shift != 0) {
-            // any shift break contiguity
+            // any shift breaks contiguity
             is_contiguous = false;
             CheckCodeBuffer(sizeof(Shift));
             *(Shift *)(code + code_cursor) = Shift(shift);
@@ -507,9 +507,22 @@ namespace PAMI
     class TypeContig : public TypeCode
     {
         public:
+            TypeContig(size_t atom_size);
             TypeContig(primitive_type_t primitive);
             ~TypeContig();
     };
+
+    inline TypeContig::TypeContig(size_t atom_size)
+        : TypeCode(true)
+    {
+        assert(0<atom_size);
+        primitive = PRIMITIVE_TYPE_CONTIGUOUS;
+        size_t prim_size = ULONG_MAX - ULONG_MAX%atom_size;
+        AddSimple(prim_size, prim_size, 1);
+        Complete();
+        SetAtomSize(atom_size);
+        AcquireReference();
+    }
 
     inline TypeContig::TypeContig(primitive_type_t primitive_type)
     {
@@ -635,5 +648,10 @@ namespace PAMI
 
   }
 }
+
+// Object of primitive PAMI_TYPE_CONTIGOUS with an atom size of 1
+// and unit size of ULONG_MAX
+extern PAMI::Type::TypeContig *PAMI_TYPE_CONTIG_MAX;
+
 
 #endif // _PAMI_TYPE_CODE_H

common/type/TypeFunc.h
@@ -21,7 +21,10 @@ namespace PAMI
       };
 
       template <typename T>
-      static void copy (void *to, void *from, size_t bytes, void *cookie) { Math::copy<T>((T*)to, (T*)from, bytes/sizeof(T)); }
+      static void copy (void *to, void *from, size_t bytes, void *cookie) {
+        // just use memcpy
+        memcpy(to, from, bytes);
+      }
 
       static void noop (void *to, void *from, size_t bytes, void *cookie) {};
 

commit 8eed8e924ab0816c933b7429a166abb23a98e82a

Issue 4526: Fix functional regression when using primitive types and primitive data type with the pami type system.
The performance improvements for contiguous type machine operations were implemented
by swapping the specified type with the special 'PAMI_TYPE_CONTIG_MAX' predefined type.
This type is essentially a 'byte' with an atom size of '1'.

Unfortunately, other type machine methods check the value of the (now swapped out) type
and alter behavior accordingly.  One method that does this is the TypeMachine::SetCopyFunc()
which specifies the data function to use with the type. The primitive dt-op math functions
are set according to the new data function and the existing (now swapped out) type for the
type machine.

To make a long story short, a 'PAMI_TYPE_DOUBLE' and 'PAMI_DATA_SUM' primitive operation
would incorrectly invoke the 'Type::Func::sum<uint8_t>' math function instead of the
'Type::Func::sum<double>' math function.

The following MPICH2 tests failed due to this regression:

  f77/rma/winaccf
  f90/attr/winaccf90
  rma/accfence2
  rma/transpose5
  rma/transpose6
  rma/test1
  rma/test1_am
  rma/test1_dt
  rma/strided_acc_indexed

These MPICH2 failures were tracked in the following BG/Q issues:

  4428
  4458
  4465
  4466
  4470
  4526

This regression was introduced with commit 8b5ef4e8d8661fd3f083f26a7ed1851a51a0ff9a

common/type/TypeMachine.h
@@ -250,7 +250,11 @@ namespace PAMI
     {
         const uintptr_t fn = (const uintptr_t) new_copy_func;
 
-        if ((type->primitive == TypeCode::PRIMITIVE_TYPE_COUNT) ||
+        TypeCode * t = type;
+        if (orig_type != NULL)
+          t = orig_type;
+
+        if ((t->primitive == TypeCode::PRIMITIVE_TYPE_COUNT) ||
             (fn >= TypeFunc::PRIMITIVE_FUNC_COUNT))
         {
           copy_func = new_copy_func;
@@ -258,7 +262,7 @@ namespace PAMI
         }
         else
         {
-          copy_func = TypeFunc::GetCopyFunction (type->primitive, (TypeFunc::primitive_func_t) fn);
+          copy_func = TypeFunc::GetCopyFunction (t->primitive, (TypeFunc::primitive_func_t) fn);
         }
     }
 